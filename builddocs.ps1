2723cb2aa94cddbbeecd4e87f54faed171cff850
Allan Ritchie <allan.ritchie@gmail.com>
2019-03-27 17:54:46 -0400

    fix ios deployment & always pull wyam

--- builddocs.ps1
+++ builddocs.ps1
@@ -1,3 +1,3 @@
-#dotnet tool install --global wyam.tool
+dotnet tool install --global wyam.tool
 iex '"c:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://localhost:5080"'
 iex 'wyam build --preview --watch'
