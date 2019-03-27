bead53ecddc47e8365759723cae5c3242b74fc34
aritchie <allan.ritchie@gmail.com>
2019-03-27 09:56:07 -0400

    protect from hostile external loggers - ALWAYS LOG somehow.  Also add events for when logs are written

--- src/Shiny.Core/Logging/Log.cs
+++ src/Shiny.Core/Logging/Log.cs
@@ -1,27 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reactive.Subjects;
 
 namespace Shiny.Logging
 {
     public static class Log
     {
+        static readonly Subject<(Exception Exception, (string Key, string Value)[] Parameters)> errorSubj = new Subject<(Exception Exception, (string Key, string Value)[] Parameters)>();
+        public static IObservable<(Exception Exception, (string Key, string Value)[] Parameters)> WhenExceptionLogged() => errorSubj;
+
+
+        static readonly Subject<(string EventName, string Description, (string Key, string Value)[] Parameters)> eventSubj = new Subject<(string EventName, string Description, (string Key, string Value)[] Parameters)>();
+        public static IObservable<(string EventName, string Description, (string Key, string Value)[] Parameters)> WhenEventLogged() => eventSubj;
 #if DEBUG
         static Log() => UseDebug();
 #endif
 
+
+
         public static void Write(Exception exception, params (string Key, string Value)[] parameters)
         {
+            errorSubj.OnNext((exception, parameters));
             foreach (var log in Loggers)
+            {
                 if (log.IsEventsEnabled)
-                    log.Log.Write(exception, parameters);
+                    DoLog(log.Log, exception, parameters);
+            }
         }
 
 
         public static void Write(string eventName, string description, params (string Key, string Value)[] parameters)
         {
+            eventSubj.OnNext((eventName, description, parameters));
             foreach (var log in Loggers)
                 if (log.IsEventsEnabled)
-                    log.Log.Write(eventName, description, parameters);
+                    DoLog(log.Log, eventName, description, parameters);
         }
 
 
@@ -41,5 +54,31 @@ namespace Shiny.Logging
 
         public static void UseConsole(bool crashesEnabled = true, bool eventsEnabled = true)
             => AddLogger(new ConsoleLogger(), crashesEnabled, eventsEnabled);
+
+
+        static void DoLog(ILogger logger, Exception ex, (string Key, string Value)[] parameters)
+        {
+            try
+            {
+                logger.Write(ex, parameters);
+            }
+            catch (Exception loggerEx)
+            {
+                Console.WriteLine($"The logger '{logger.GetType().FullName}' failed with {loggerEx}");
+            }
+        }
+
+
+        static void DoLog(ILogger logger, string eventName, string description, (string Key, string Value)[] parameters)
+        {
+            try
+            {
+                logger.Write(eventName, description, parameters);
+            }
+            catch (Exception loggerEx)
+            {
+                Console.WriteLine($"The logger '{logger.GetType().FullName}' failed with {loggerEx}");
+            }
+        }
     }
 }
