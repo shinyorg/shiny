1dee09d1c3887f96632593e553b7ba65c24e80d0
aritchie <allan.ritchie@gmail.com>
2019-03-30 08:41:55 -0400

    notes

--- builddocs.ps1
+++ builddocs.ps1
@@ -1,3 +1,5 @@
+# add below to update
+# dotnet tool uninstall --global wyam.tool
 dotnet tool install --global wyam.tool
-iex '"c:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://localhost:5080"'
+# iex '"c:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://localhost:5080"'
 iex 'wyam build --preview --watch'
