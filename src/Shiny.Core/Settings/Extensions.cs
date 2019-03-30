85abcd426f61a02905565c7f03227ff400260f0b
aritchie <allan.ritchie@gmail.com>
2019-03-30 16:06:02 -0400

    settings extensions

---
+++ src/Shiny.Core/Settings/Extensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace Shiny.Settings
+{
+    public static class Extensions
+    {
+        static readonly object syncLock = new object();
+
+
+        /// <summary>
+        /// Thread safetied setting value incrementor
+        /// </summary>
+        /// <param name="settings"></param>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static int IncrementValue(this ISettings settings, string name = "NextId")
+        {
+            var id = 0;
+
+            lock (syncLock)
+            {
+                id = settings.Get(name, 0);
+                id++;
+                settings.Set(name, id);
+            }
+            return id;
+        }
+    }
+}
