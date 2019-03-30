73f92df9d238ec5001387613db5490e6f957bb14
aritchie <allan.ritchie@gmail.com>
2019-03-30 16:04:31 -0400

    Power extensions

--- src/Shiny.Core/Power/Extensions.cs
+++ src/Shiny.Core/Power/Extensions.cs
@@ -5,6 +5,29 @@ namespace Shiny.Power
 {
     public static class Extensions
     {
+        /// <summary>
+        /// Sets up an easy power status observable
+        /// </summary>
+        /// <param name="power"></param>
+        /// <returns></returns>
+        public static IObservable<PowerState> WhenStatusChanged(this IPowerManager power)
+            => power.WhenAnyProperty(x => x.Status);
+
+
+        /// <summary>
+        /// Sets up an easy battery level observable
+        /// </summary>
+        /// <param name="power"></param>
+        /// <returns></returns>
+        public static IObservable<int> WhenBatteryLevelChanged(this IPowerManager power)
+            => power.WhenAnyProperty(x => x.BatteryLevel);
+
+
+        /// <summary>
+        /// Returns true if any power state indicates the power is plugged in
+        /// </summary>
+        /// <param name="power"></param>
+        /// <returns></returns>
         public static bool IsPluggedIn(this IPowerManager power) =>
             power.Status == PowerState.Charged ||
             power.Status == PowerState.Charging ||
