27f25224eb0484fa36c5f98d93965961e6eab6a9
aritchie <allan.ritchie@gmail.com>
2019-03-30 08:41:49 -0400

    add docs

--- src/Shiny.Core/IStartupTask.cs
+++ src/Shiny.Core/IStartupTask.cs
@@ -4,11 +4,14 @@
 namespace Shiny
 {
     /// <summary>
-    /// Registering this in your Shiny.Startup will run these tasks immediately after the
+    /// Registering this in your Shiny.Startup (using RegisterStartupTask<T>) will run these tasks immediately after the
     /// service container has been built
     /// </summary>
     public interface IStartupTask
     {
+        /// <summary>
+        /// This method is immediately executed after the container is built (so the class implementing this interface supports constructor DI)
+        /// </summary>
         void Start();
     }
 }
