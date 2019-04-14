3b113a30214183305f42cdb6d633e06916ee705c
Allan Ritchie <allan.ritchie@gmail.com>
2019-04-14 10:36:28 -0400

    Metric fix

--- src/Shiny.Net.Http/MetricExtensions.cs
+++ src/Shiny.Net.Http/MetricExtensions.cs
@@ -5,23 +5,27 @@ using System.Reactive.Linq;
 
 namespace Shiny.Net.Http
 {
+    class MetricHolder
+    {
+        public string Identifier { get; set; }
+        public long LastBytesTransferred { get; set; }
+        public DateTime LastPing { get; set; }
+    }
+
+
     public static class MetricExtensions
     {
         public static IObservable<HttpTransferMetric> WithMetrics(this IObservable<HttpTransfer> transfers)
         {
-            var startTime = DateTime.UtcNow;
-            var metrics = new Dictionary<string, (DateTime LastPing, long LastBytesTransferred)>();
+            var metrics = new Dictionary<string, MetricHolder>();
 
             return transfers.Select(transfer =>
             {
-                //var metric = AddOrGet(transfer, metrics);
-                //metric.BytesPerSecond = 0;
-                //metric.EstimatedCompletionTime = TimeSpan.Zero;
-
+                var metric = default(HttpTransferMetric);
                 switch (transfer.Status)
                 {
                     case HttpTransferState.InProgress:
-                        //ApplyMetric(transfer, metric);
+                        metric = Calculate(transfer, metrics);
                         break;
 
                     case HttpTransferState.Cancelled:
@@ -32,60 +36,48 @@ namespace Shiny.Net.Http
                             metrics.Remove(transfer.Identifier);
                         break;
                 }
-                //return metric;
-                return default(HttpTransferMetric);
+                return metric;
             });
         }
 
 
-        //static void ApplyMetric(HttpTransfer transfer, HttpTransferMetric metric)
-        //{
-        //    if (transfer.FileSize <= 0 || transfer.BytesTransferred <= 0)
-        //        return;
+        static HttpTransferMetric Calculate(HttpTransfer transfer, IDictionary<string, MetricHolder> metrics)
+        {
+            lock (metrics)
+            {
+                var metric = default(HttpTransferMetric);
 
-        //    // I don't have a start to calc from yet
-        //    if (metric.LastChanged == null)
-        //    {
-        //        metric.LastChanged = DateTime.UtcNow;
-        //        metric.LastBytesTransferred = transfer.BytesTransferred;
-        //    }
-        //    else
-        //    {
-        //        var elapsed = DateTime.UtcNow - metric.LastChanged.Value;
-        //        var totalSeconds = (long)elapsed.TotalSeconds;
+                if (!metrics.ContainsKey(transfer.Identifier))
+                {
+                    metrics.Add(transfer.Identifier, new MetricHolder
+                    {
+                        LastBytesTransferred = transfer.BytesTransferred,
+                        LastPing = DateTime.UtcNow
+                    });
+                }
+                else if (transfer.BytesTransferred > 0 && transfer.FileSize > 0)
+                {
+                    var holder = metrics[transfer.Identifier];
+                    var elapsed = DateTime.UtcNow - holder.LastPing;
+                    var totalSeconds = (long)elapsed.TotalSeconds;
 
-        //        // sampling rate of 2 seconds
-        //        if (totalSeconds >= 2)
-        //        {
-        //            var xferDiff = transfer.BytesTransferred - metric.LastBytesTransferred;
-        //            var bytesPerSecond = xferDiff / totalSeconds;
-        //            var rawEta = (transfer.FileSize - transfer.BytesTransferred) / bytesPerSecond;
+                    // sampling rate of 2 seconds
+                    if (totalSeconds >= 2)
+                    {
+                        var xferDiff = transfer.BytesTransferred - holder.LastBytesTransferred;
+                        var bytesPerSecond = xferDiff / totalSeconds;
+                        var rawEta = (transfer.FileSize - transfer.BytesTransferred) / bytesPerSecond;
 
-        //            //Console.WriteLine($"b/s: {bytesPerSecond} - ETA: {rawEta}");
-        //            //metric.BytesPerSecond = Convert.ToInt64(bytesPerSecond);
-        //            //metric.EstimatedCompletionTime = TimeSpan.FromSeconds(rawEta);
-        //            metric.LastChanged = DateTime.UtcNow;
-        //            metric.LastBytesTransferred = transfer.BytesTransferred;
-        //        }
-        //    }
-        //}
-
-
-        //static HttpTransferMetrics AddOrGet(IHttpTransfer transfer, IDictionary<string, HttpTransferMetrics> metrics)
-        //{
-        //    lock (metrics)
-        //    {
-        //        HttpTransferMetrics metric = null;
-        //        if (metrics.ContainsKey(transfer.Identifier))
-        //            metric = metrics[transfer.Identifier];
-        //        else
-        //        {
-        //            metric = new HttpTransferMetrics { Transfer = transfer };
-        //            metrics.Add(transfer.Identifier, metric);
-        //        }
-        //        return metric;
-        //    }
-        //}
-
+                        //Console.WriteLine($"b/s: {bytesPerSecond} - ETA: {rawEta}");
+                        //metric.BytesPerSecond = Convert.ToInt64(bytesPerSecond);
+                        //metric.EstimatedCompletionTime = TimeSpan.FromSeconds(rawEta);
+                        metric = new HttpTransferMetric(transfer, bytesPerSecond, TimeSpan.FromSeconds(rawEta));
+                    }
+                    holder.LastPing = DateTime.UtcNow;
+                    holder.LastBytesTransferred = transfer.BytesTransferred;
+                }
+                return metric;
+            }
+        }
     }
 }
