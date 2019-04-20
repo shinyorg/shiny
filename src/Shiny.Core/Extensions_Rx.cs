cdf7e75abe1f3dc987cf4a5a1dc83d1769c28697
aritchie <allan.ritchie@gmail.com>
2019-04-20 08:18:24 -0400

    add disposewith method for when rxui isn't around

--- src/Shiny.Core/Extensions_Rx.cs
+++ src/Shiny.Core/Extensions_Rx.cs
@@ -27,6 +27,20 @@ namespace Shiny
     public static partial class Extensions
     {
         /// <summary>
+        /// A function from ReactiveUI - useful for non-ui stuff too ;)
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="this"></param>
+        /// <param name="compositeDisposable"></param>
+        /// <returns></returns>
+        public static T DisposeWith<T>(this T @this, CompositeDisposable compositeDisposable) where T : IDisposable
+        {
+            compositeDisposable.Add(@this);
+            return @this;
+        }
+
+
+        /// <summary>
         /// A handy way for replying & completing an observer - common for single valued observables
         /// </summary>
         /// <typeparam name="T"></typeparam>
