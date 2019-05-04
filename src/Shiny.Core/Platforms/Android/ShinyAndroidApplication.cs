fcb4fe0d3137e316810394a9f1ddbc472c75e549
aritchie <allan.ritchie@gmail.com>
2019-05-04 07:17:26 -0400

    make shinyandroidapplication usable for other scenarios

--- src/Shiny.Core/Platforms/Android/ShinyAndroidApplication.cs
+++ src/Shiny.Core/Platforms/Android/ShinyAndroidApplication.cs
@@ -6,7 +6,23 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Shiny
 {
-    public abstract class ShinyAndroidApplication<T> : Application where T : Startup, new()
+    public abstract class ShinyAndroidApplication : Application
+    {
+        protected ShinyAndroidApplication(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer) { }
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
+            AndroidShinyHost.Init(this, null, this.OnBuildApplication);
+        }
+
+
+        protected virtual void OnBuildApplication(IServiceCollection builder) { }
+    }
+
+
+
+    public abstract class ShinyAndroidApplication<T> : Application where T : IStartup, new()
     {
         protected ShinyAndroidApplication(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer) {}
 
