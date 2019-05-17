f5f93d4bb6698390de288296c89bea56c19a7bf4
Allan Ritchie <allan.ritchie@gmail.com>
2019-05-17 10:53:38 -0400

    notification extension

---
+++ src/Shiny.Notifications/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+
+namespace Shiny.Notifications
+{
+    public static class Extensions
+    {
+        public static Task Send(this INotificationManager notifications, string title, string message)
+            => notifications.Send(new Notification
+            {
+                Title = title,
+                Message = message
+            });
+    }
+}
