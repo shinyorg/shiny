6a157a48fd86ca5502ac33e07cecedaa86ec5193
Allan Ritchie <allan.ritchie@gmail.com>
2019-05-19 20:55:02 -0400

    start fixing universal beacons

--- src/Shiny.Beacons/Platforms/Shared/BeaconExtensions.cs
+++ src/Shiny.Beacons/Platforms/Shared/BeaconExtensions.cs
@@ -1,5 +1,6 @@
 ﻿#if WINDOWS_UWP || __ANDROID__
 using System;
+using System.Linq;
 using System.Reactive.Linq;
 using Shiny.BluetoothLE.Central;
 
@@ -21,24 +22,56 @@ namespace Shiny.Beacons
             if (md == null)
                 return false;
 
-            //if (md.CompanyId != 39)
-            //    return false;
+            if (md.CompanyId != 76)
+                return false;
+
+            if (md.Data.Length != 23)
+                return false;
 
             return true;
-            //if (data.Length < 25)
-            //    return false;
-
-            //// apple manufacturerID - https://www.bluetooth.com/specifications/assigned-numbers/company-Identifiers
-            //if (data[0] != 76 || data[1] != 0)
-            //    return false;
-
-            //return true;
         }
 
 
         public static Beacon ToBeacon(this IScanResult result)
         {
-            return null;
+            var data = result.AdvertisementData.ManufacturerData.Data;
+            Console.WriteLine("RAW: " + BitConverter.ToString(data));
+
+            var uuidString = BitConverter.ToString(data, 2, 16).Replace("-", String.Empty);
+            var uuid = new Guid(uuidString);
+            var major = BitConverter.ToUInt16(data.Skip(18).Take(2).ToArray(), 0);
+            var minor = BitConverter.ToUInt16(data.Skip(20).Take(2).ToArray(), 0);
+            var txpower = data[22];
+            var accuracy = CalculateAccuracy(txpower, result.Rssi);
+            var proximity = CalculateProximity(accuracy);
+
+            return new Beacon(uuid, major, minor, accuracy, proximity);
+        }
+
+
+        public static double CalculateAccuracy(int txpower, double rssi)
+        {
+            var ratio = rssi * 1 / Math.Abs(txpower);
+            if (ratio < 1.0)
+                return Math.Pow(ratio, 10);
+
+            var accuracy = 0.89976 * Math.Pow(ratio, 7.7095) + 0.111;
+            return accuracy;
+        }
+
+
+        public static Proximity CalculateProximity(double accuracy)
+        {
+            if (accuracy < 0)
+                return Proximity.Unknown;
+
+            if (accuracy < 0.5)
+                return Proximity.Immediate;
+
+            if (accuracy <= 4.0)
+                return Proximity.Near;
+
+            return Proximity.Far;
         }
     }
 }
