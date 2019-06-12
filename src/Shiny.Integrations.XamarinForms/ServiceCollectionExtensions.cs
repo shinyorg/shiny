e3a2d7c7bca4e681fdbf1fb9d1efd0e3b43c7eaf
Allan Ritchie <allan.ritchie@gmail.com>
2019-06-12 09:02:54 -0400

    xamforms dependency service integration

---
+++ src/Shiny.Integrations.XamarinForms/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Xamarin.Forms;
+
+
+namespace Shiny
+{
+    public static class ServicesCollectionExtensions
+    {
+        public static void UseXamarinFormsDependencyService(this IServiceCollection services)
+        {
+            services.RegisterPostBuildAction(sp =>
+            {
+                foreach (var service in services)
+                {
+                    var instance = sp.GetService(service.ServiceType);
+                    var method = typeof(DependencyService).GetMethod("Register");
+                    var generic = method.MakeGenericMethod(new [] { service.ServiceType });
+                    generic.Invoke(null, new [] { instance });
+                }
+            });
+        }
+    }
+}
