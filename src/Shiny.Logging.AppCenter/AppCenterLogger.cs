6929d58e3377c943bbb48f81b065c0316f5ccc5c
Allan Ritchie <allan.ritchie@gmail.com>
2019-06-28 18:26:03 -0400

    shiny appcenter logging

---
+++ src/Shiny.Logging.AppCenter/AppCenterLogger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using Shiny.Logging;
+
+
+namespace Shiny
+{
+    public class AppCenterLogger : ILogger
+    {
+        public void Write(Exception exception, params (string Key, string Value)[] parameters)
+            => Crashes.TrackError(exception, parameters.ToDictionary());
+
+
+        public void Write(string eventName, string description, params (string Key, string Value)[] parameters)
+            => Analytics.TrackEvent($"[{eventName}] {description}", parameters.ToDictionary());
+    }
+}
