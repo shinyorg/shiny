d1c9b7743f492aec6efca165f8c21de31a2b6d53
Allan Ritchie <allan.ritchie@gmail.com>
2019-07-04 20:34:45 -0400

    minor

--- src/Shiny.Integrations.XamEssentials/ServiceCollectionExtensions.cs
+++ src/Shiny.Integrations.XamEssentials/ServiceCollectionExtensions.cs
@@ -6,7 +6,14 @@ namespace Shiny
 {
     public static class ServicesCollectionExtensions
     {
+#if __ANDROID__
         public static void Use(this IServiceCollection services)
+        {
+
+        }
+#endif
+
+        public static void UseXamEssentialsConnectivity(this IServiceCollection services)
         {
 
         }
