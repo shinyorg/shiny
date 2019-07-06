237fd123a20d0d4a4724888484d43e3337645b8d
Allan Ritchie <allan.ritchie@gmail.com>
2019-07-06 15:17:55 -0400

    working ideas for xf integration

---
+++ src/Shiny.Integrations.XamarinForms/Platforms/iOS/ShinyFormsAppDelegate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Foundation;
+using UIKit;
+using Xamarin.Forms.Platform.iOS;
+
+
+namespace Shiny
+{
+    public class ShinyFormsAppDelegate : FormsApplicationDelegate
+    {
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            //iOSShinyHost.Init(new SampleStartup());
+            return base.FinishedLaunching(app, options);
+        }
+    }
+}
