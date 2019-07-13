83da837ab35f5468df05b9339150c40ca7c3ff4d
Allan Ritchie <allan.ritchie@gmail.com>
2019-07-13 09:02:44 -0500

    ShinyModule

--- src/Shiny.Core/ShinyModule.cs
+++ src/Shiny.Core/ShinyModule.cs
@@ -4,14 +4,14 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Shiny
 {
-    public interface IModule
+    public interface IShinyModule
     {
         void Register(IServiceCollection services);
         void OnContainerReady(IServiceProvider services);
     }
 
 
-    public abstract class Module : IModule
+    public abstract class ShinyModule : IShinyModule
     {
         public abstract void Register(IServiceCollection services);
         public virtual void OnContainerReady(IServiceProvider services) { }
