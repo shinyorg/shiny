46e9d65aa6f5040a09a0b20554443f90fd8675ca
Allan Ritchie <allan.ritchie@gmail.com>
2019-07-13 11:54:38 -0500

    tizen notifications stubbed

---
+++ src/Shiny.Notifications/Platforms/Tizen/NotificationManagerImpl.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace Shiny.Notifications
+{
+    public class NotificationManagerImpl : INotificationManager
+    {
+        public Task Cancel(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Notification>> GetPending()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AccessState> RequestAccess()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Send(Notification notification)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
