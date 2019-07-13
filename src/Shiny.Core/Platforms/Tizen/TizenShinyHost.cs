ef1e2e095847bb3bfd54ba7bb5056211ab2213df
Allan Ritchie <allan.ritchie@gmail.com>
2019-07-13 08:06:30 -0500

    Create TizenShinyHost.cs

---
+++ src/Shiny.Core/Platforms/Tizen/TizenShinyHost.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Shiny
+{
+    public class TizenShinyHost : ShinyHost
+    {
+        public static void Init(IShinyStartup startup = null, Action<IServiceCollection> platformBuild = null)
+            => InitPlatform(startup, services =>
+            {
+                //services.AddSingleton<IEnvironment, EnvironmentImpl>();
+                //services.AddSingleton<IConnectivity, ConnectivityImpl>();
+                //services.AddSingleton<IPowerManager, PowerManagerImpl>();
+                //services.AddSingleton<IJobManager, JobManager>();
+                //services.AddSingleton<IRepository, FileSystemRepositoryImpl>();
+                //services.AddSingleton<IFileSystem, FileSystemImpl>();
+                //services.AddSingleton<ISettings, SettingsImpl>();
+                platformBuild?.Invoke(services);
+            });
+    }
+}
