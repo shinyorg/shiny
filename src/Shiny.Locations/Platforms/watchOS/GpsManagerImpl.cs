e351d9e3aa5a3f5426f4182f4ff577cbaa39b80d
Allan Ritchie <allan.ritchie@gmail.com>
2019-07-16 13:27:13 -0400

    stub locations for tizen and watchos

---
+++ src/Shiny.Locations/Platforms/watchOS/GpsManagerImpl.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Shiny.Locations
+{
+    public class GpsManagerImpl : IGpsManager
+    {
+        public bool IsListening => throw new NotImplementedException();
+
+        public AccessState GetCurrentStatus(bool background)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IObservable<IGpsReading> GetLastReading()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AccessState> RequestAccess(bool backgroundMode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StartListener(GpsRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StopListener()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IObservable<AccessState> WhenAccessStatusChanged(bool forBackground)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IObservable<IGpsReading> WhenReading()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
