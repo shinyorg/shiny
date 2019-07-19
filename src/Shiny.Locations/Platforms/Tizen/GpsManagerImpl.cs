d63380a5060e376c896f6cd541ebf1ec26eae753
Allan Ritchie <allan.ritchie@gmail.com>
2019-07-18 20:57:30 -0400

    more tizen & watchos wip

--- src/Shiny.Locations/Platforms/Tizen/GpsManagerImpl.cs
+++ src/Shiny.Locations/Platforms/Tizen/GpsManagerImpl.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Threading;
+using System.Reactive.Linq;
 using System.Threading.Tasks;
 using Tizen.Location;
 
@@ -8,55 +8,76 @@ namespace Shiny.Locations
 {
     public class GpsManagerImpl : IGpsManager
     {
+        readonly Locator locator;
+        static Location lastKnownLocation = new Location();
+
+
+        public GpsManagerImpl()
+        {
+            var locatorType = LocationType.Gps;
+            var gps = Platform.Get<bool>("location.gps");
+            var wps = Platform.Get<bool>("location.wps");
+            if (gps)
+            {
+                locatorType = wps ? LocationType.Hybrid : LocationType.Gps;
+            }
+            else
+            {
+                locatorType = wps ? LocationType.Wps : LocationType.Passive;
+            }
+            this.locator = new Locator(locatorType);
+        }
+
+        public bool IsListening => false;
+
+        public AccessState GetCurrentStatus(bool background)
+        {
+            //this.locator.DistanceBasedLocationChanged
+            throw new NotImplementedException();
+        }
+
+        public IObservable<IGpsReading> GetLastReading()
+            => Observable.Return(lastKnownLocation);
+
+        public Task<AccessState> RequestAccess(bool backgroundMode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StartListener(GpsRequest request)
+        {
+            this.locator.Start();
+            return Task.CompletedTask;
+        }
+
+
+        public Task StopListener()
+        {
+            this.locator.Stop();
+            return Task.CompletedTask;
+        }
+
+
+        public IObservable<AccessState> WhenAccessStatusChanged(bool forBackground)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public IObservable<IGpsReading> WhenReading() => Observable.Create<IGpsReading>(ob =>
+        {
+            var handler = new EventHandler<LocationChangedEventArgs>((sender, args) =>
+            {
+
+            });
+            this.locator.LocationChanged += handler;
+            return () => this.locator.LocationChanged -= handler;
+        });
     }
 }
 
 
 
-
-//namespace Xamarin.Essentials
-//{
-//    public static partial class Geolocation
-//    {
-//        static Location lastKnownLocation = new Location();
-
-//        static async Task<Location> PlatformLastKnownLocationAsync()
-//        {
-//            await Permissions.RequestAsync(PermissionType.LocationWhenInUse);
-
-//            return lastKnownLocation;
-//        }
-
-//        static async Task<Location> PlatformLocationAsync(GeolocationRequest request, CancellationToken cancellationToken)
-//        {
-//            await Permissions.RequireAsync(PermissionType.LocationWhenInUse);
-
-//            Locator service = null;
-//            var gps = Platform.GetFeatureInfo<bool>("location.gps");
-//            var wps = Platform.GetFeatureInfo<bool>("location.wps");
-//            if (gps)
-//            {
-//                if (wps)
-//                    service = new Locator(LocationType.Hybrid);
-//                else
-//                    service = new Locator(LocationType.Gps);
-//            }
-//            else
-//            {
-//                if (wps)
-//                    service = new Locator(LocationType.Wps);
-//                else
-//                    service = new Locator(LocationType.Passive);
-//            }
-
-//            var tcs = new TaskCompletionSource<bool>();
-
-//            cancellationToken = Utils.TimeoutToken(cancellationToken, request.Timeout);
-//            cancellationToken.Register(() =>
-//            {
-//                service?.Stop();
-//                tcs.TrySetResult(false);
-//            });
 
 //            double KmToMetersPerSecond(double km) => km * 0.277778;
 //            service.LocationChanged += (s, e) =>
@@ -71,14 +92,3 @@ namespace Shiny.Locations
 //                    lastKnownLocation.Speed = KmToMetersPerSecond(e.Location.Speed);
 //                    lastKnownLocation.Timestamp = e.Location.Timestamp;
 //                }
-//                service?.Stop();
-//                tcs.TrySetResult(true);
-//            };
-//            service.Start();
-
-//            await tcs.Task;
-
-//            return lastKnownLocation;
-//        }
-//    }
-//}
---
+++ src/Shiny.Locations/Platforms/Tizen/GpsReading.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Tizen.Location;
+
+namespace Shiny.Locations
+{
+    public class GpsReading : IGpsReading
+    {
+        readonly Location location;
+        public GpsReading(Location location)
+            => this.location = location;
+
+
+        public double Altitude => this.location.Altitude;
+        public double Heading => this.location.Direction;
+        public double HeadingAccuracy => this.location.Accuracy;
+        public double Speed => throw new NotImplementedException();
+        public Position Position => throw new NotImplementedException();
+        public double PositionAccuracy => throw new NotImplementedException();
+        public DateTime Timestamp => this.location.Timestamp;
+    }
+}
