f452d49673d2600c49ab95bff8147a316f06f4a2
Allan Ritchie <allan.ritchie@gmail.com>
2019-07-19 21:51:24 -0400

    Safety broadcast receiver

--- src/Shiny.Locations/Platforms/Android/GeofenceBroadcastReceiver.cs
+++ src/Shiny.Locations/Platforms/Android/GeofenceBroadcastReceiver.cs
@@ -8,8 +8,7 @@ namespace Shiny.Locations
 {
     [BroadcastReceiver(
         Name = "com.shiny.locations.GeofenceBroadcastReceiver",
-        Exported = true,
-        DirectBootAware = true
+        Exported = true
     )]
     [IntentFilter(new [] {
         "com.shiny.locations.GeofenceBroadcastReceiver.ACTION_PROCESS",
@@ -17,25 +16,20 @@ namespace Shiny.Locations
     })]
     public class GeofenceBroadcastReceiver : BroadcastReceiver
     {
+        // startup tasks replace this, but this receiver is still used to trigger the wakeup on reboot
         public const string INTENT_ACTION = "com.shiny.locations.GeofenceBroadcastReceiver.ACTION_PROCESS";
 
 
         public override void OnReceive(Context context, Intent intent) => this.Execute(async () =>
         {
+            if (intent.Action != INTENT_ACTION)
+                return;
+
             var geofences = ShinyHost.Resolve<IGeofenceManager>() as IAndroidGeofenceManager;
             if (geofences == null)
                 throw new ArgumentException("Invalid AndroidGeofenceManager");
 
-            switch (intent.Action)
-            {
-                case INTENT_ACTION:
-                    await geofences.Process(intent);
-                    break;
-
-                case Permission.ReceiveBootCompleted:
-                    // startup tasks replace this, but this receiver is still used to trigger the wakeup on reboot
-                    break;
-            }
+            await geofences.Process(intent);
         });
     }
 }
