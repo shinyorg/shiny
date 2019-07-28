cfd1ed4ab320fc27008243b79379f10ab256b18f
aritchie <allan.ritchie@gmail.com>
2019-07-28 16:49:08 -0500

    remove for devops

--- src/Shiny.Core/Jobs/Platforms/iOS/BgTasksJobManager.cs
+++ src/Shiny.Core/Jobs/Platforms/iOS/BgTasksJobManager.cs
@@ -1,87 +1,87 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
-using BackgroundTasks;
-using CoreFoundation;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Threading;
+//using System.Threading.Tasks;
+//using BackgroundTasks;
+//using CoreFoundation;
 
-namespace Shiny.Jobs
-{
-    public class BgTasksJobManager : IJobManager
-    {
-        public bool IsRunning => throw new NotImplementedException();
+//namespace Shiny.Jobs
+//{
+//    public class BgTasksJobManager : IJobManager
+//    {
+//        public bool IsRunning => throw new NotImplementedException();
 
-        public event EventHandler<JobInfo> JobStarted;
-        public event EventHandler<JobRunResult> JobFinished;
+//        public event EventHandler<JobInfo> JobStarted;
+//        public event EventHandler<JobRunResult> JobFinished;
 
-        public Task Cancel(string jobName)
-        {
-            throw new NotImplementedException();
-        }
+//        public Task Cancel(string jobName)
+//        {
+//            throw new NotImplementedException();
+//        }
 
-        public Task CancelAll()
-        {
-            BGTaskScheduler.Shared.CancelAll();
-            return Task.CompletedTask;
-        }
+//        public Task CancelAll()
+//        {
+//            BGTaskScheduler.Shared.CancelAll();
+//            return Task.CompletedTask;
+//        }
 
-        public Task<JobInfo> GetJob(string jobIdentifier)
-        {
+//        public Task<JobInfo> GetJob(string jobIdentifier)
+//        {
 
-            throw new NotImplementedException();
-        }
+//            throw new NotImplementedException();
+//        }
 
-        public Task<IEnumerable<JobInfo>> GetJobs()
-        {
-            throw new NotImplementedException();
-        }
+//        public Task<IEnumerable<JobInfo>> GetJobs()
+//        {
+//            throw new NotImplementedException();
+//        }
 
-        public Task<AccessState> RequestAccess()
-        {
-            throw new NotImplementedException();
-        }
+//        public Task<AccessState> RequestAccess()
+//        {
+//            throw new NotImplementedException();
+//        }
 
-        public Task<JobRunResult> Run(string jobIdentifier, CancellationToken cancelToken = default)
-        {
-            throw new NotImplementedException();
-        }
+//        public Task<JobRunResult> Run(string jobIdentifier, CancellationToken cancelToken = default)
+//        {
+//            throw new NotImplementedException();
+//        }
 
-        public Task<IEnumerable<JobRunResult>> RunAll(CancellationToken cancelToken = default)
-        {
-            throw new NotImplementedException();
-        }
+//        public Task<IEnumerable<JobRunResult>> RunAll(CancellationToken cancelToken = default)
+//        {
+//            throw new NotImplementedException();
+//        }
 
-        public void RunTask(string taskName, Func<CancellationToken, Task> task)
-        {
-            throw new NotImplementedException();
-        }
+//        public void RunTask(string taskName, Func<CancellationToken, Task> task)
+//        {
+//            throw new NotImplementedException();
+//        }
 
-        public Task Schedule(JobInfo jobInfo)
-        {
-            //BGProcessingTask
-            //BGTask
-            //BGTaskScheduler
-            //BGAppRefreshTask
-            //BGAppRefreshTaskRequest
+//        public Task Schedule(JobInfo jobInfo)
+//        {
+//            //BGProcessingTask
+//            //BGTask
+//            //BGTaskScheduler
+//            //BGAppRefreshTask
+//            //BGAppRefreshTaskRequest
 
-            var request = new BGProcessingTaskRequest(jobInfo.Identifier)
-            {
-                RequiresExternalPower = jobInfo.DeviceCharging,
-                RequiresNetworkConnectivity = jobInfo.RequiredInternetAccess != InternetAccess.None
-                //EarliestBeginDate
-            };
-            if (!BGTaskScheduler.Shared.Submit(request, out var error))
-                throw new ArgumentException(error.LocalizedDescription);
-            //var request = new BGProcessingTaskRequest()
-            //BGTaskScheduler.Shared.Register(
-            //    jobInfo.Identifier,
-            //    DispatchQueue.DefaultGlobalQueue,
-            //    task =>
-            //    {
-            //    }
-            //);
+//            var request = new BGProcessingTaskRequest(jobInfo.Identifier)
+//            {
+//                RequiresExternalPower = jobInfo.DeviceCharging,
+//                RequiresNetworkConnectivity = jobInfo.RequiredInternetAccess != InternetAccess.None
+//                //EarliestBeginDate
+//            };
+//            if (!BGTaskScheduler.Shared.Submit(request, out var error))
+//                throw new ArgumentException(error.LocalizedDescription);
+//            //var request = new BGProcessingTaskRequest()
+//            //BGTaskScheduler.Shared.Register(
+//            //    jobInfo.Identifier,
+//            //    DispatchQueue.DefaultGlobalQueue,
+//            //    task =>
+//            //    {
+//            //    }
+//            //);
 
-            return Task.CompletedTask;
-        }
-    }
-}
+//            return Task.CompletedTask;
+//        }
+//    }
+//}
