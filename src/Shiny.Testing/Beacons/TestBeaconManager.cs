0691248a97a11c03007f01045453b534c8a707d2
aritchie <allan.ritchie@gmail.com>
2019-07-28 14:02:19 -0500

    add beaconmanager mock to testing lib

---
+++ src/Shiny.Testing/Beacons/TestBeaconManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Subjects;
+using System.Threading.Tasks;
+using Shiny.Beacons;
+
+
+namespace Shiny.Testing.Beacons
+{
+    public class BeaconManager : IBeaconManager
+    {
+        readonly IList<BeaconRegion> monitoredRegions = new List<BeaconRegion>();
+
+
+        AccessState accessStatus = AccessState.Available;
+        public AccessState AccessStatus {
+            get => this.accessStatus;
+            set
+            {
+                this.accessStatus = value;
+                this.accessStateSubject.OnNext(value);
+            }
+        }
+
+
+        public AccessState GetCurrentStatus(bool forMonitoring) => this.AccessStatus;
+        public Task<IEnumerable<BeaconRegion>> GetMonitoredRegions() => Task.FromResult(this.monitoredRegions.AsEnumerable());
+        public Task<AccessState> RequestAccess(bool monitoring) => Task.FromResult(this.AccessStatus);
+
+
+        public Task StartMonitoring(BeaconRegion region)
+        {
+            this.monitoredRegions.Add(region);
+            return Task.CompletedTask;
+        }
+
+        public Task StopAllMonitoring()
+        {
+            this.monitoredRegions.Clear();
+            return Task.CompletedTask;
+        }
+
+
+        public Task StopMonitoring(BeaconRegion region)
+        {
+            this.monitoredRegions.Remove(region);
+            return Task.CompletedTask;
+        }
+
+
+        readonly Subject<AccessState> accessStateSubject = new Subject<AccessState>();
+        public IObservable<AccessState> WhenAccessStatusChanged(bool monitoring) => this.accessStateSubject;
+
+
+        public Subject<Beacon> BeaconRangedSubject { get; } = new Subject<Beacon>();
+        public IObservable<Beacon> WhenBeaconRanged(BeaconRegion region) => this.BeaconRangedSubject;
+    }
+}
