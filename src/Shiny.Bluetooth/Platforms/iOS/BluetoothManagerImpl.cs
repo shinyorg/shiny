44e35e42c50bad4f218b81567e1ef121084ee11d
aritchie <allan.ritchie@gmail.com>
2019-07-29 21:28:02 -0500

    Update BluetoothManagerImpl.cs

--- src/Shiny.Bluetooth/Platforms/iOS/BluetoothManagerImpl.cs
+++ src/Shiny.Bluetooth/Platforms/iOS/BluetoothManagerImpl.cs
@@ -1,9 +1,26 @@
 ﻿using System;
-
+using CoreBluetooth;
+using Foundation;
 
 namespace Shiny.Bluetooth
 {
-    public class BluetoothManagerImpl : IBluetoothManager
+    public class BluetoothManagerImpl : CBCentralManagerDelegate, IBluetoothManager
     {
+        readonly CBCentralManager manager;
+
+
+        public BluetoothManagerImpl()
+        {
+            this.manager = new CBCentralManager();
+        }
+
+
+        public override void DiscoveredPeripheral(CBCentralManager central, CBPeripheral peripheral, NSDictionary advertisementData, NSNumber RSSI)
+        {
+        }
+
+        public override void UpdatedState(CBCentralManager central)
+        {
+        }
     }
 }
