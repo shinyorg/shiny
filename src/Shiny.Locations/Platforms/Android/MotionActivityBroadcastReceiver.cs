d50a066b91e831dee97d3d2284f32970c0b599d3
Allan Ritchie <allan.ritchie@gmail.com>
2019-08-30 17:33:29 -0400

    Update MotionActivityBroadcastReceiver.cs

--- src/Shiny.Locations/Platforms/Android/MotionActivityBroadcastReceiver.cs
+++ src/Shiny.Locations/Platforms/Android/MotionActivityBroadcastReceiver.cs
@@ -22,20 +22,60 @@ namespace Shiny.Locations
         public override void OnReceive(Context context, Intent intent) => this.Execute(async () =>
         {
             // DELETE FROM motion_activity WHERE Timestamp < DateTimeOffset.UtcNow.AddDays(-30).Ticks
-            if (ActivityRecognitionResult.HasResult(intent))
-            {
-                var result = ActivityRecognitionResult.ExtractResult(intent);
-                foreach (var activity in result.ProbableActivities)
-                {
-                    var confidence = 0;
-                    var type = (int) MotionActivityType.Unknown;
-                    var timestamp = DateTime.UtcNow.Ticks;
+            if (!ActivityRecognitionResult.HasResult(intent))
+                return;
 
-                    await this.database.ExecuteNonQuery(
-                        $"INSERT INTO motion_activity(Event, Confidence, Timestamp) VALUES ({type}, {confidence}, {timestamp})"
-                    );
+            var result = ActivityRecognitionResult.ExtractResult(intent);
+            var type = MotionActivityType.Unknown;
+
+            foreach (var activity in result.ProbableActivities)
+            {
+                switch (activity.Type)
+                {
+                    case DetectedActivity.InVehicle:
+                        type |= MotionActivityType.Automotive;
+                        break;
+
+                    case DetectedActivity.OnBicycle:
+                        type |= MotionActivityType.Cycling;
+                        break;
+
+                    case DetectedActivity.OnFoot:
+                        type |= MotionActivityType.Walking;
+                        break;
+
+                    case DetectedActivity.Running:
+                        type |= MotionActivityType.Running;
+                        break;
+
+                    case DetectedActivity.Still:
+                        type |= MotionActivityType.Stationary;
+                        break;
+
+                    case DetectedActivity.Walking:
+                        type |= MotionActivityType.Walking;
+                        break;
                 }
             }
+            var confidence = this.ToConfidence(result.MostProbableActivity.Confidence);
+            var timestamp = DateTime.UtcNow.Ticks;
+
+            await this.database.ExecuteNonQuery(
+                $"INSERT INTO motion_activity(Event, Confidence, Timestamp) VALUES ({type}, {confidence}, {timestamp})"
+            );
+            this.messageBus.Publish(new MotionActivityEvent(type, MotionActivityConfidence.High, DateTimeOffset.UtcNow));
         });
+
+
+        protected virtual MotionActivityConfidence ToConfidence(int value)
+        {
+            if (value >= 70)
+                return MotionActivityConfidence.High;
+
+            if (value >= 40)
+                return MotionActivityConfidence.Medium;
+
+            return MotionActivityConfidence.Low;
+        }
     }
 }
