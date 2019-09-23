26a5cbe9439a5adc123e16b969d78be6076bc4c0
Allan Ritchie <allan.ritchie@gmail.com>
2019-09-23 14:30:20 -0400

    fixes for .net core 3 updates

--- src/Shiny.Integrations.SysTextJson/SysTextJsonSerializer.cs
+++ src/Shiny.Integrations.SysTextJson/SysTextJsonSerializer.cs
@@ -1,14 +1,14 @@
 ﻿using System;
 using Shiny.Infrastructure;
-using System.Text.Json.Serialization;
+using System.Text.Json;
 
 
 namespace Shiny.Integrations.SysTextJson
 {
     public class SysTextJsonSerializer : ISerializer
     {
-        public T Deserialize<T>(string value) => JsonSerializer.Parse<T>(value);
-        public object Deserialize(Type objectType, string value) => JsonSerializer.Parse(value, objectType);
-        public string Serialize(object value) => JsonSerializer.ToString(value);
+        public T Deserialize<T>(string value) => JsonSerializer.Deserialize<T>(value);
+        public object Deserialize(Type objectType, string value) => JsonSerializer.Serialize(value, objectType);
+        public string Serialize(object value) => JsonSerializer.Serialize(value);
     }
 }
