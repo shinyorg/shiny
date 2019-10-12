cde3f7abcf9ca785a96b08aace0b96994361bdca
Allan Ritchie <allan.ritchie@gmail.com>
2019-10-12 12:56:23 -0400

    start of auto registration module

---
+++ src/Shiny.Core/Infrastructure/AutoRegisterAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Shiny.Infrastructure
+{
+
+    [AttributeUsage(AttributeTargets.Assembly)]
+    public abstract class AutoRegisterAttribute : Attribute
+    {
+        public abstract void Register(IServiceCollection services);
+    }
+}
---
+++ src/Shiny.Core/Infrastructure/AutoStartup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Shiny.Infrastructure
+{
+    public class AutoStartup : ShinyStartup
+    {
+        public override void ConfigureServices(IServiceCollection services)
+        {
+            services.RegisterModule(new AutoRegistrationModule());
+        }
+    }
+}
