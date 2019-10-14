f11ac80a3e09ec50e7ab31cc9545d67b8df7720d
Allan Ritchie <allan.ritchie@gmail.com>
2019-10-14 15:21:03 -0400

    shiny & sqlite integrations now have registration attributes

---
+++ src/Shiny.Integrations.Sqlite/ShinySqliteIntegrationAttribute.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Shiny.Infrastructure;
+
+
+namespace Shiny
+{
+    public class ShinySqliteIntegrationAttribute : ServiceModuleAttribute
+    {
+        public ShinySqliteIntegrationAttribute(bool useCache = false,
+                                               bool useSettings = false,
+                                               bool useRepository = false,
+                                               bool crashLogging = false,
+                                               bool eventLogging = false)
+        {
+            this.UseCache = useCache;
+            this.UseSettings = useSettings;
+            this.UseRepository = useRepository;
+            this.UseCrashLogging = crashLogging;
+            this.UseEventLogging = eventLogging;
+        }
+
+
+        public bool UseCache { get; set; }
+        public bool UseSettings { get; set; }
+        public bool UseRepository { get; set; }
+        public bool UseCrashLogging { get; set; }
+        public bool UseEventLogging { get; set; }
+
+
+        public override void Register(IServiceCollection services)
+        {
+            if (this.UseEventLogging || this.UseCrashLogging)
+                services.UseSqliteLogging(this.UseCrashLogging, this.UseEventLogging);
+
+            if (this.UseCache)
+                services.UseSqliteCache();
+
+            if (this.UseSettings)
+                services.UseSqliteSettings();
+
+            if (this.UseRepository)
+                services.UseSqliteStorage();
+        }
+    }
+}
---
+++ src/Shiny.Logging.AppCenter/ShinyAppCenterIntegrationAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Shiny.Infrastructure;
+
+
+namespace Shiny
+{
+    public class ShinyAppCenterIntegrationAttribute : ServiceModuleAttribute
+    {
+        public ShinyAppCenterIntegrationAttribute(string appSecret = null, bool logCrashes = true, bool logEvents = false)
+        {
+            this.AppSecret = appSecret;
+            this.LogCrashes = logCrashes;
+            this.LogEvents = logEvents;
+        }
+
+
+        public string AppSecret { get; set; }
+        public bool LogCrashes { get; set; }
+        public bool LogEvents { get; set; }
+        public override void Register(IServiceCollection services)
+            => services.UseAppCenterLogging(this.AppSecret, this.LogCrashes, this.LogEvents);
+    }
+}
