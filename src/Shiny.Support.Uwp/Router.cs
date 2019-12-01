9e13ebb7899e97e8bb14d1d27e5d65859b6fe4f2
Allan Ritchie <allan.ritchie@gmail.com>
2019-12-01 17:57:01 -0500

    nope

---
+++ src/Shiny.Support.Uwp/Router.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Windows.ApplicationModel.Background;
+
+
+namespace Shiny
+{
+    static class Router
+    {
+        public static void Run(IBackgroundTaskInstance taskInstance, string classType, string methodName)
+        {
+            var host = Type.GetType(classType);
+            var method = host.GetMethod(methodName);
+            method.Invoke(host, new[] { taskInstance });
+        }
+    }
+}
