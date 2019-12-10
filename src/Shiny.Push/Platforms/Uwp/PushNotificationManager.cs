e3941f5ef92d26805b26b65295d98f6f00b8f339
Allan Ritchie <allan.ritchie@gmail.com>
2019-12-09 21:18:58 -0500

    Update PushNotificationManager.cs

--- src/Shiny.Push/Platforms/Uwp/PushNotificationManager.cs
+++ src/Shiny.Push/Platforms/Uwp/PushNotificationManager.cs
@@ -1,13 +1,30 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Windows.Networking.PushNotifications;
+using Shiny.Logging;
+
 
 namespace Shiny.Push
 {
     public class PushNotificationManager : IPushNotificationManager
     {
-        public Task<PushAccessState> RequestAccess()
+        readonly IServiceProvider serviceProvider;
+        public PushNotificationManager(IServiceProvider serviceProvider) => this.serviceProvider = serviceProvider;
+
+
+        public async Task<PushAccessState> RequestAccess()
         {
-            throw new NotImplementedException();
+            var channel = await PushNotificationChannelManager.CreatePushNotificationChannelForApplicationAsync();
+            //channel.ExpirationTime - persist and deal with this
+            channel.PushNotificationReceived += this.OnPushNotification;
+            return new PushAccessState(AccessState.Available, channel.Uri);
         }
+
+
+        void OnPushNotification(PushNotificationChannel sender, PushNotificationReceivedEventArgs e) => Log.SafeExecute(async () =>
+        {
+            await this.serviceProvider.Resolve<IPushNotificationDelegate>()?.OnReceived(e.RawNotification.Content);
+            e.Cancel = true;
+        });
     }
 }
