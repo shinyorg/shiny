cc3400e9cbb8c18a5ca17224d2fa54d06a901fd6
Allan Ritchie <allan.ritchie@gmail.com>
2020-01-20 21:39:52 -0500

    minor

--- src/Shiny.Net.Http/ShinyHttpTransfers.cs
+++ src/Shiny.Net.Http/ShinyHttpTransfers.cs
@@ -11,10 +11,10 @@ namespace Shiny
         static IHttpTransferManager Current { get; } = ShinyHost.Resolve<IHttpTransferManager>();
 
         public static Task Cancel(string id) => Current.Cancel(id);
-        public static Task Cancel(QueryFilter filter = null) => Current.Cancel(filter);
+        public static Task Cancel(QueryFilter? filter = null) => Current.Cancel(filter);
         public static Task<HttpTransfer> Enqueue(HttpTransferRequest request) => Current.Enqueue(request);
         public static Task<HttpTransfer> GetTransfer(string id) => Current.GetTransfer(id);
-        public static Task<IEnumerable<HttpTransfer>> GetTransfers(QueryFilter filter = null) => Current.GetTransfers(filter);
+        public static Task<IEnumerable<HttpTransfer>> GetTransfers(QueryFilter? filter = null) => Current.GetTransfers(filter);
         public static IObservable<HttpTransfer> WhenUpdated() => Current.WhenUpdated();
     }
 }
