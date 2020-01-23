709d2252bec769d1f2953cef5006ea4c539dcd3e
Allan Ritchie <allan.ritchie@gmail.com>
2020-01-23 17:14:43 -0500

    Always set minimum bg fetch time and let user configure otherwise

--- src/Shiny.Core/Jobs/Platforms/iOS/JobManager.cs
+++ src/Shiny.Core/Jobs/Platforms/iOS/JobManager.cs
@@ -19,6 +19,8 @@ namespace Shiny.Jobs
 
         public JobManager(IServiceProvider container, IRepository repository) : base(container, repository)
         {
+            if (PlatformExtensions.HasBackgroundMode("fetch"))
+                UIApplication.SharedApplication.SetMinimumBackgroundFetchInterval(BackgroundFetchInterval ?? UIApplication.BackgroundFetchIntervalMinimum);
             //UIApplication.SharedApplication.ObserveValue(UIApplication.BackgroundRefreshStatusDidChangeNotification)
         }
 
