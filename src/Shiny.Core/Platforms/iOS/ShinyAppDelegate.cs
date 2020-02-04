6ad53a3de7d697be2f6c7fc8df9f8baedb3c22fd
Allan Ritchie <allan.ritchie@gmail.com>
2020-02-04 10:03:21 -0500

    uwp updates

--- src/Shiny.Core/Platforms/iOS/ShinyAppDelegate.cs
+++ src/Shiny.Core/Platforms/iOS/ShinyAppDelegate.cs
@@ -16,23 +16,14 @@ namespace Shiny
 
         protected virtual void RegisterPlatformServices(IServiceCollection services) { }
 
-        public override void OnActivated(UIApplication application)
-            => iOSShinyHost.OnForeground();
-
-        public override void OnResignActivation(UIApplication uiApplication)
-            => iOSShinyHost.OnBackground();
-
-        public override void WillTerminate(UIApplication uiApplication)
-            => iOSShinyHost.OnTerminate();
+        public override void PerformFetch(UIApplication application, Action<UIBackgroundFetchResult> completionHandler)
+            => iOSShinyHost.PerformFetch(completionHandler);
 
         public override void RegisteredForRemoteNotifications(UIApplication application, NSData deviceToken)
             => iOSShinyHost.RegisteredForRemoteNotifications(deviceToken);
 
         public override void FailedToRegisterForRemoteNotifications(UIApplication application, NSError error)
             => iOSShinyHost.FailedToRegisterForRemoteNotifications(error);
-
-        public override void PerformFetch(UIApplication application, Action<UIBackgroundFetchResult> completionHandler)
-            => iOSShinyHost.PerformFetch(completionHandler);
 
         public override void HandleEventsForBackgroundUrl(UIApplication application, string sessionIdentifier, Action completionHandler)
             => iOSShinyHost.HandleEventsForBackgroundUrl(sessionIdentifier, completionHandler);
