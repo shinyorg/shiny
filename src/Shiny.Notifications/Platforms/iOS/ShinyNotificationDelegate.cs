91419e0bfe0dbcedd0a508a2198c7ab5ec7cb4b0
Allan Ritchie <allan.ritchie@gmail.com>
2020-02-17 13:17:25 -0500

    Update ShinyNotificationDelegate.cs

--- src/Shiny.Notifications/Platforms/iOS/ShinyNotificationDelegate.cs
+++ src/Shiny.Notifications/Platforms/iOS/ShinyNotificationDelegate.cs
@@ -7,14 +7,38 @@ namespace Shiny.Notifications
     public class ShinyNotificationDelegate : UNUserNotificationCenterDelegate
     {
         public override void DidReceiveNotificationResponse(UNUserNotificationCenter center, UNNotificationResponse response, Action completionHandler)
-        {
+            => Log.SafeExecute(async () =>
+            {
+                var notification = response.Notification.Request.FromNative();
+                if (notification == null)
+                    return;
 
-        }
+                var sdelegate = ShinyHost.Resolve<INotificationDelegate>();
+                if (x.Response is UNTextInputNotificationResponse textResponse)
+                {
+                    var shinyResponse = new NotificationResponse(notification, textResponse.ActionIdentifier, textResponse.UserText);
+                    await sdelegate.OnEntry(shinyResponse);
+                }
+                else
+                {
+                    var shinyResponse = new NotificationResponse(notification, x.Response.ActionIdentifier, null);
+                    await sdelegate.OnEntry(shinyResponse);
+                }
+            })
+            .ContinueWith(_ => completionHandler());
 
 
         public override void WillPresentNotification(UNUserNotificationCenter center, UNNotification notification, Action<UNNotificationPresentationOptions> completionHandler)
-        {
-
-        }
+            => Log.SafeExecute(() =>
+            {
+                var shinyNotification = x.Notification.Request.FromNative();
+                if (shinyNotification != null)
+                {
+                    await ShinyHost
+                        .Resolve<INotificationDelegate>()
+                        .OnReceived(shinyNotification);
+                }
+            })
+            .ContinueWith(_ => completionHandler(UNNotificationPresentationOptions.Alert));
     }
 }
