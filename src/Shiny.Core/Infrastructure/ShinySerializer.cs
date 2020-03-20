d84339c34d947467e17b53a7702c60dddbe578f2
Allan Ritchie <allan.ritchie@gmail.com>
2020-03-20 11:48:06 -0400

    minor

--- src/Shiny.Core/Infrastructure/ShinySerializer.cs
+++ src/Shiny.Core/Infrastructure/ShinySerializer.cs
@@ -1,14 +1,17 @@
 ﻿using System;
-using Shiny.Infrastructure;
-using System.Text.Json;
+using Newtonsoft.Json;
+//using System.Text.Json;
 
 
 namespace Shiny.Infrastructure
 {
     public class ShinySerializer : ISerializer
     {
-        public T Deserialize<T>(string value) => JsonSerializer.Deserialize<T>(value);
-        public object Deserialize(Type objectType, string value) => JsonSerializer.Serialize(value, objectType);
-        public string Serialize(object value) => JsonSerializer.Serialize(value);
+        public T Deserialize<T>(string value) => JsonConvert.DeserializeObject<T>(value);
+        public object Deserialize(Type objectType, string value) => JsonConvert.DeserializeObject(value, objectType);
+        public string Serialize(object value) => JsonConvert.SerializeObject(value);
+        //public T Deserialize<T>(string value) => JsonSerializer.Deserialize<T>(value);
+        //public object Deserialize(Type objectType, string value) => JsonSerializer.Serialize(value, objectType);
+        //public string Serialize(object value) => JsonSerializer.Serialize(value);
     }
 }
