d0455e5e41d6a7241773ebc3ec4876b68481292f
Allan Ritchie <allan.ritchie@gmail.com>
2020-05-31 17:05:04 -0400

    Update LocationSyncManager.cs

--- src/Shiny.Locations.Sync/LocationSyncManager.cs
+++ src/Shiny.Locations.Sync/LocationSyncManager.cs
@@ -7,16 +7,23 @@ using Shiny.Jobs;
 
 namespace Shiny.Locations.Sync
 {
+    public enum LocationSyncType
+    {
+        GPS,
+        Geofence
+    }
+
+
     public interface ILocationSyncManager
     {
-        Task ForceRun();
+        Task ForceRun(LocationSyncType? syncType = null);
 
-        // TODO: what if a geofence is removed?  let user deal with it?
+        Task<SyncConfig?> GetConfig(LocationSyncType syncType);
+        Task SetConfig(LocationSyncType syncType, SyncConfig config);
+        Task ClearEvents(LocationSyncType? syncType = null);
+
         Task<IList<GeofenceEvent>> GetPendingGeofenceEvents();
-        Task ClearGeofenceEvents();
-
         Task<IList<GpsEvent>> GetPendingGpsEvents();
-        Task ClearGpsEvents();
     }
 
 
@@ -33,14 +40,80 @@ namespace Shiny.Locations.Sync
         }
 
 
-        public Task ForceRun() => Task.WhenAll
-        (
-            this.jobManager.RunJobAsTask(Constants.GeofenceJobIdentifer),
-            this.jobManager.RunJobAsTask(Constants.GpsJobIdentifier)
-        );
-        public Task ClearGeofenceEvents() => this.repository.Clear<GeofenceEvent>();
-        public Task ClearGpsEvents() => this.repository.Clear<GpsEvent>();
+        public async Task ForceRun(LocationSyncType? syncType = null) 
+        {
+            switch (syncType)
+            {
+                case LocationSyncType.Geofence:
+                    await this.jobManager.RunJobAsTask(Constants.GeofenceJobIdentifer);
+                    break;
+
+                case LocationSyncType.GPS:
+                    await this.jobManager.RunJobAsTask(Constants.GpsJobIdentifier);
+                    break;
+
+                default:
+                    await Task.WhenAll
+                    (
+                        this.jobManager.RunJobAsTask(Constants.GeofenceJobIdentifer),
+                        this.jobManager.RunJobAsTask(Constants.GpsJobIdentifier)
+                    );
+                    break;
+            }
+        }
+
+            
         public Task<IList<GeofenceEvent>> GetPendingGeofenceEvents() => this.repository.GetAll<GeofenceEvent>();
         public Task<IList<GpsEvent>> GetPendingGpsEvents() => this.repository.GetAll<GpsEvent>();
+
+
+        static string GetKey(LocationSyncType syncType) => syncType == LocationSyncType.Geofence
+            ? Constants.GeofenceJobIdentifer
+            : Constants.GpsJobIdentifier;
+
+
+        public async Task<SyncConfig?> GetConfig(LocationSyncType syncType)
+        {
+            var key = GetKey(syncType);
+            var job = await this.jobManager.GetJob(key);
+            if (job == null)
+                return null;
+
+            return job.GetParameter<SyncConfig>(Constants.SyncConfigJobParameterKey);
+        }
+
+
+        public async Task SetConfig(LocationSyncType syncType, SyncConfig config)
+        {
+            var key = GetKey(syncType);
+            var job = await this.jobManager.GetJob(key);
+            if (job == null)
+                throw new ArgumentException("LocationSync type is not active");
+
+            job.SetParameter(Constants.SyncConfigJobParameterKey, config);
+            await this.jobManager.Schedule(job);
+        }
+
+
+        public async Task ClearEvents(LocationSyncType? syncType = null)
+        {
+            switch (syncType)
+            {
+                case LocationSyncType.Geofence:
+                    await this.repository.Clear<GeofenceEvent>();
+                    break;
+
+                case LocationSyncType.GPS:
+                    await this.repository.Clear<GpsEvent>();
+                    break;
+
+                default:
+                    await Task.WhenAll(
+                        this.repository.Clear<GeofenceEvent>(),
+                        this.repository.Clear<GpsEvent>()
+                    );
+                    break;
+            }
+        }
     }
 }
