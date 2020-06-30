0980e0a88a24bdfbc91d48873d06f4e198263d53
Allan Ritchie <allan.ritchie@gmail.com>
2020-06-29 22:14:52 -0400

    minor

---
+++ tests/Shiny.Tests/TripTracker/TripTrackerDelegateTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace Shiny.Tests.TripTracker
+{
+    public class TripTrackerDelegateTests
+    {
+        [Fact]
+        public async Task NewTripCreated()
+        {
+
+        }
+
+
+        [Fact]
+        public async Task TripClosedWhenActivityChanges()
+        {
+
+        }
+
+
+        [Fact]
+        public async Task TripDistanceCalcuatedProperly()
+        {
+        }
+    }
+}
