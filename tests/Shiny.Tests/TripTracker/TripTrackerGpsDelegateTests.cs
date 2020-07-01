a57e7151dcfba68ca6afa586f777d046e483aa2a
Allan Ritchie <allan.ritchie@gmail.com>
2020-06-30 23:37:09 -0400

    Update TripTrackerGpsDelegateTests.cs

--- tests/Shiny.Tests/TripTracker/TripTrackerGpsDelegateTests.cs
+++ tests/Shiny.Tests/TripTracker/TripTrackerGpsDelegateTests.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Moq;
+using Shiny.Locations;
+using Shiny.TripTracker;
+using Shiny.TripTracker.Internals;
 using Xunit;
 
 
@@ -7,6 +11,29 @@ namespace Shiny.Tests.TripTracker
 {
     public class TripTrackerGpsDelegateTests
     {
+        readonly Mock<IDataService> data;
+        readonly Mock<IMotionActivityManager> activityManager;
+        readonly Mock<ITripTrackerManager> manager;
+        readonly Mock<IGpsReading> gpsReading;
+        readonly TripTrackerGpsDelegate gpsDelegate;
+
+
+        public TripTrackerGpsDelegateTests()
+        {
+            this.manager = new Mock<ITripTrackerManager>();
+            this.activityManager = new Mock<IMotionActivityManager>();
+            this.data = new Mock<IDataService>();
+            this.gpsReading = new Mock<IGpsReading>();
+
+            this.gpsDelegate = new TripTrackerGpsDelegate(
+                this.manager.Object,
+                this.activityManager.Object,
+                this.data.Object,
+                null
+            );
+        }
+
+
         [Fact]
         public async Task NewTripCreated()
         {
