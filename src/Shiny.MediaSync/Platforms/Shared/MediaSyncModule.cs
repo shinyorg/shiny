15d1ce1f0e80faf4d6ef57e500f2c0ea3d26e12a
Allan Ritchie <allan.ritchie@gmail.com>
2020-07-03 14:11:41 -0400

    Update MediaSyncModule.cs

--- src/Shiny.MediaSync/Platforms/Shared/MediaSyncModule.cs
+++ src/Shiny.MediaSync/Platforms/Shared/MediaSyncModule.cs
@@ -9,22 +9,16 @@ namespace Shiny.MediaSync
     public class MediaSyncModule : ShinyModule
     {
         readonly Type delegateType;
-
-
         public MediaSyncModule(Type? delegateType)
-        {
-            this.delegateType = delegateType ?? typeof(MediaSyncDelegate);
-        }
+            => this.delegateType = delegateType ?? typeof(MediaSyncDelegate);
 
 
         public override void Register(IServiceCollection services)
         {
             services.AddSingleton(typeof(IMediaSyncDelegate), this.delegateType);
             services.AddSingleton<IMediaSyncManager, MediaSyncManagerImpl>();
-            services.RegisterJob(
-                typeof(SyncJob),
-                runInForeground: true
-            );
+            services.AddSingleton<IDataService, SqliteDataService>();
+            services.RegisterJob(typeof(SyncJob), runInForeground: true);
             services.UseNotifications(true);
             services.UseHttpTransfers<MediaSyncHttpTransferDelegate>();
             services.AddSingleton<IMediaGalleryScanner, MediaGalleryScannerImpl>();
