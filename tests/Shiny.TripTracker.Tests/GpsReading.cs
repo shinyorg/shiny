a69b5f64d08c6dde0adb980a2e0e10044344b767
Allan Ritchie <allan.ritchie@gmail.com>
2020-07-07 14:06:41 -0400

    Some unit tests

---
+++ tests/Shiny.TripTracker.Tests/GpsReading.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Shiny.Locations;
+
+
+namespace Shiny.TripTracker.Tests
+{
+    public class GpsReading : IGpsReading
+    {
+        public static GpsReading Create(double lat, double lng) => new GpsReading
+        {
+            Position = new Position(lat, lng)
+        };
+
+
+        public double Altitude { get; set; }
+        public double Heading { get; set; }
+        public double HeadingAccuracy { get; set; }
+        public double Speed { get; set; }
+        public Position Position { get; set; }
+        public double PositionAccuracy { get; set; }
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+    }
+}
--- tests/Shiny.TripTracker.Tests/TripTrackerGpsDelegateTests.cs
+++ tests/Shiny.TripTracker.Tests/TripTrackerGpsDelegateTests.cs
@@ -1,21 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using FluentAssertions;
 using Moq;
 using Shiny.Locations;
-using Shiny.TripTracker;
 using Shiny.TripTracker.Internals;
 using Xunit;
 
 
-namespace Shiny.Tests.TripTracker
+namespace Shiny.TripTracker.Tests
 {
     public class TripTrackerGpsDelegateTests
     {
         readonly Mock<IDataService> data;
         readonly Mock<IMotionActivityManager> activityManager;
         readonly Mock<ITripTrackerManager> manager;
-        readonly Mock<IGpsReading> gpsReading;
         readonly TripTrackerGpsDelegate gpsDelegate;
+        readonly List<TripCheckin> checkins = new List<TripCheckin>();
+        MotionActivityType? tracking = MotionActivityType.Automotive;
+        MotionActivityType currentMotionType = MotionActivityType.Automotive;
 
 
         public TripTrackerGpsDelegateTests()
@@ -23,7 +26,45 @@ namespace Shiny.Tests.TripTracker
             this.manager = new Mock<ITripTrackerManager>();
             this.activityManager = new Mock<IMotionActivityManager>();
             this.data = new Mock<IDataService>();
-            this.gpsReading = new Mock<IGpsReading>();
+
+            this.manager
+                .Setup(x => x.TrackingActivityTypes)
+                .Returns(() => this.tracking);
+
+            this.activityManager
+                .Setup(x => x.Query(It.IsAny<DateTimeOffset>(), It.IsAny<DateTimeOffset>()))
+                .Returns(() => Task.FromResult<IList<MotionActivityEvent>>(new List<MotionActivityEvent> 
+                {
+                    new MotionActivityEvent(
+                        this.currentMotionType, 
+                        MotionActivityConfidence.High, 
+                        DateTimeOffset.UtcNow
+                    )
+                }));
+
+
+            this.data
+                .Setup(x => x.Checkin(
+                    It.IsAny<int>(), 
+                    It.IsAny<IGpsReading>()
+                ))
+                .Returns(Task.CompletedTask);
+
+            this.data
+                .Setup(x => x.GetCheckinsByTrip(It.IsAny<int>()))
+                .Returns(() => Task.FromResult<IList<TripCheckin>>(this.checkins));
+
+            this.data
+                .Setup(x => x.Checkin(It.IsAny<int>(), It.IsAny<IGpsReading>()))
+                .Callback<int, IGpsReading>((tripId, reading) => this.checkins.Add(new TripCheckin
+                {
+                    TripId = tripId,
+                    Direction = reading.Heading,
+                    Latitude = reading.Position.Latitude,
+                    Longitude = reading.Position.Longitude,
+                    DateCreated = DateTimeOffset.Now
+                }))
+                .Returns(Task.CompletedTask);
 
             this.gpsDelegate = new TripTrackerGpsDelegate(
                 this.manager.Object,
@@ -37,13 +78,34 @@ namespace Shiny.Tests.TripTracker
         [Fact]
         public async Task NewTripCreated()
         {
+            Trip? result = null;
+            this.data
+                .Setup(x => x.Save(It.IsAny<Trip>()))
+                .Callback<Trip>(x => result = x)
+                .Returns(Task.CompletedTask);
+
+            await this.gpsDelegate.OnReading(GpsReading.Create(1, 1));
+            result.Should().NotBeNull();
+            result.DateFinished.Should().BeNull();
         }
 
 
         [Fact]
         public async Task TripClosedWhenActivityChanges()
         {
+            var trip = new Trip
+            {
+                Id = 1,
+                TripType = MotionActivityType.Automotive
+            };
+            this.gpsDelegate.CurrentTripId = 1;
+            this.data
+                .Setup(x => x.GetTrip(It.IsAny<int>()))
+                .Returns(Task.FromResult(trip));
 
+            this.currentMotionType = MotionActivityType.Cycling;
+            await this.gpsDelegate.OnReading(GpsReading.Create(1, 1));
+            trip.DateFinished.Should().NotBeNull();
         }
 
 
