e57f75e95f01820058b4f313211435c47a4f8700
Allan Ritchie <allan.ritchie@gmail.com>
2020-07-14 08:55:44 -0400

    Stubbing out thoughts on printers

---
+++ src/Shiny.Printers.Abstractions/IPrinter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reactive;
+
+
+namespace Shiny.Printers
+{
+    public interface IPrinter
+    {
+        void Queue(string id, string content); // TODO: watch progress on queue?
+        void RemoveQueue(string id);
+        IObservable<Unit> Print(string content); // TODO: progress?
+        IObservable<Unit> Connect();
+        void Disconnect();
+    }
+}
---
+++ src/Shiny.Printers.Abstractions/IPrinterManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+
+namespace Shiny.Printers
+{
+    public interface IPrinterManager
+    {
+        IObservable<IPrinter> Scan();
+        Task<AccessState> RequestAccess();
+    }
+}
