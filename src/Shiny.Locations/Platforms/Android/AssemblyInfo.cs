f629fec61097ed88871e0de76dadd7ed40e8ac97
Allan Ritchie <allan.ritchie@gmail.com>
2020-07-22 21:40:27 -0400

    Add location background permission to droid

--- src/Shiny.Locations/Platforms/Android/AssemblyInfo.cs
+++ src/Shiny.Locations/Platforms/Android/AssemblyInfo.cs
@@ -3,4 +3,5 @@ using Android.App;
 
 [assembly: UsesPermission(Android.Manifest.Permission.AccessFineLocation)]
 [assembly: UsesPermission(Android.Manifest.Permission.AccessCoarseLocation)]
+[assembly: UsesPermission(Android.Manifest.Permission.AccessBackgroundLocation)]
 [assembly: UsesPermission("com.google.android.gms.permission.ACTIVITY_RECOGNITION")]
