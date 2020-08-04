09574b4e5dffae896199ab8709c9ae69606b87f7
Allan Ritchie <allan.ritchie@gmail.com>
2020-08-04 14:47:23 -0400

    Minor

---
+++ src/Shiny.BluetoothLE.Abstractions/Extensions_Async.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace Shiny.BluetoothLE
+{
+    public static class AsyncExtensions
+    {
+
+        public static Task ConnectWaitAsync(this IPeripheral peripheral, CancellationToken? cancelToken = null)
+            => peripheral
+                .ConnectWait()
+                .Timeout(TimeSpan.FromSeconds(30))
+                .ToTask(cancelToken ?? CancellationToken.None);
+
+        public static Task<IGattService> GetServicesAsync(this IPeripheral peripheral, CancellationToken? cancelToken = null)
+            => peripheral.DiscoverServices().ToTask(cancelToken ?? CancellationToken.None);
+    }
+}
