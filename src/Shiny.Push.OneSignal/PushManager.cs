590bf75eef109988bb6206754135e4fff032dd22
Allan Ritchie <allan.ritchie@gmail.com>
2020-08-04 10:52:56 -0400

    Prep onesignal push

---
+++ src/Shiny.Push.OneSignal/PushManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Shiny.Settings;
+
+
+namespace Shiny.Push.OneSignal
+{
+    public class PushManager : Shiny.Push.PushManager, IPushTagSupport
+    {
+       
+    }
+}
---
+++ src/Shiny.Push.OneSignal/ServiceCollectionExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Shiny.Notifications;
+using Shiny.Push;
+
+
+namespace Shiny
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static bool UseOneSignalPush(this IServiceCollection services,
+                                            Type delegateType,
+                                            params NotificationCategory[] categories)
+        {
+#if NETSTANDARD2_0
+            return false;
+#else
+            //services.RegisterModule(new PushModule(
+            //    typeof(Shiny.Push.AzureNotificationHubs.PushManager),
+            //    delegateType,
+            //    categories
+            //));
+            //services.AddSingleton(new Shiny.Push.AzureNotificationHubs.AzureNotificationConfig(listenerConnectionString, hubName));
+            return true;
+#endif
+        }
+
+
+        public static bool UseOneSignalPush<TPushDelegate>(this IServiceCollection services,
+                                                           params NotificationCategory[] categories)
+            where TPushDelegate : class, IPushDelegate
+            => services.UseOneSignalPush(
+                typeof(TPushDelegate),
+                categories
+            );
+    }
+}
