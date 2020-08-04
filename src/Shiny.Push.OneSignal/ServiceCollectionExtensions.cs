cbe6864c4e547da1a8fd14dc26a8494d4ac40bd0
Allan Ritchie <allan.ritchie@gmail.com>
2020-08-04 15:15:57 -0400

    minor

--- src/Shiny.Push.OneSignal/ServiceCollectionExtensions.cs
+++ src/Shiny.Push.OneSignal/ServiceCollectionExtensions.cs
@@ -35,3 +35,78 @@ namespace Shiny
             );
     }
 }
+
+//https://documentation.onesignal.com/docs/xamarin-sdk-setup
+
+//<application....>
+
+//  <receiver android:name="com.onesignal.GcmBroadcastReceiver"
+//            android:permission="com.google.android.c2dm.permission.SEND" >
+//    <intent-filter>
+//      <action android:name="com.google.android.c2dm.intent.RECEIVE" />
+//      <category android:name="${manifestApplicationId}" />
+//    </intent-filter>
+//  </receiver>
+
+//</application>
+//MainPage = new OneSignalXamarinFormsExamplePage();
+
+////Remove this method to stop OneSignal Debugging
+//OneSignal.Current.SetLogLevel(LOG_LEVEL.VERBOSE, LOG_LEVEL.NONE);
+
+//  OneSignal.Current.StartInit("YOUR_ONESIGNAL_APP_ID")
+//  .Settings(new Dictionary<string, bool>() {
+//    { IOSSettings.kOSSettingsKeyAutoPrompt, false },
+//    { IOSSettings.kOSSettingsKeyInAppLaunchURL, false } })
+//  .InFocusDisplaying(OSInFocusDisplayOption.Notification)
+//  .EndInit();
+
+//// The promptForPushNotificationsWithUserResponse function will show the iOS push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission (See step 7)
+//OneSignal.Current.RegisterForPushNotifications();
+
+
+
+
+//using System;
+//using Foundation;
+//using UIKit;
+//using UserNotifications;
+//using Com.OneSignal;
+//using Com.OneSignal.Abstractions;
+
+//namespace OneSignalNotificationServiceExtension
+//{
+//    [Register("NotificationService")]
+//    public class NotificationService : UNNotificationServiceExtension
+//    {
+//        Action<UNNotificationContent> ContentHandler { get; set; }
+//        UNMutableNotificationContent BestAttemptContent { get; set; }
+//        UNNotificationRequest ReceivedRequest { get; set; }
+
+//        protected NotificationService(IntPtr handle) : base(handle)
+//        {
+//            // Note: this .ctor should not contain any initialization logic.
+//        }
+
+//        public override void DidReceiveNotificationRequest(UNNotificationRequest request, Action<UNNotificationContent> contentHandler)
+//        {
+//            ReceivedRequest = request;
+//            ContentHandler = contentHandler;
+//            BestAttemptContent = (UNMutableNotificationContent)request.Content.MutableCopy();
+
+//            (OneSignal.Current as OneSignalImplementation).DidReceiveNotificationExtensionRequest(request, BestAttemptContent);
+
+//            ContentHandler(BestAttemptContent);
+//        }
+
+//        public override void TimeWillExpire()
+//        {
+//            // Called just before the extension will be terminated by the system.
+//            // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+
+//            (OneSignal.Current as OneSignalImplementation).ServiceExtensionTimeWillExpireRequest(ReceivedRequest, BestAttemptContent);
+
+//            ContentHandler(BestAttemptContent);
+//        }
+//    }
+//}
