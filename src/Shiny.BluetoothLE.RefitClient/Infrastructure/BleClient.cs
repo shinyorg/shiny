cf16e3ccd5a37915414bc76bb0fe30cbde7d12f5
Allan Ritchie <allan.ritchie@gmail.com>
2020-08-05 10:10:13 -0400

    Update BleClient.cs

--- src/Shiny.BluetoothLE.RefitClient/Infrastructure/BleClient.cs
+++ src/Shiny.BluetoothLE.RefitClient/Infrastructure/BleClient.cs
@@ -8,7 +8,7 @@ namespace Shiny.BluetoothLE.RefitClient.Infrastructure
     public class BleClient : IBleClient
     {
         public IPeripheral Peripheral { get; internal set; }
-        protected IBleDataSerializer Serializer { get; set; }
+        public IBleDataSerializer Serializer { get; set; }
 
         public IObservable<Unit> Connect() => this.Peripheral.ConnectWait().Select(_ => Unit.Default);
         public void Disconnect() => this.Peripheral.CancelConnection();
