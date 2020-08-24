0d3faf9b8a0ad2656f3c55cbfd646120bbe460f8
Allan Ritchie <allan.ritchie@gmail.com>
2020-08-24 09:51:08 -0400

    Fix #270 - ability to listen to other notification types on UWP

--- src/Shiny.Push/Platforms/Uwp/PushNotificationBackgroundTaskProcessor.cs
+++ src/Shiny.Push/Platforms/Uwp/PushNotificationBackgroundTaskProcessor.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Windows.ApplicationModel.Background;
 using Windows.Networking.PushNotifications;
+using Windows.UI.Notifications;
 
 
 namespace Shiny.Push
@@ -18,21 +19,32 @@ namespace Shiny.Push
 
         public async void Process(IBackgroundTaskInstance taskInstance)
         {
+            // TODO: push delegate OnEntry?
             var deferral = taskInstance.GetDeferral();
-            var notification = taskInstance.TriggerDetails as RawNotification;
+            var fire = true;
+            IDictionary<string, string> headers = new Dictionary<string, string>();
 
-            if (notification != null)
+            if (taskInstance.TriggerDetails is RawNotification raw)
             {
-                // TODO: push delegate OnEntry?
-                var headers = notification
-                    .Headers?
-                    .ToDictionary(
-                        x => x.Key,
-                        x => x.Value
-                    ) ?? new Dictionary<string, string>(0);
-
-                await this.serviceProvider.RunDelegates<IPushDelegate>(x => x.OnReceived(headers));
+                if (raw.Headers != null)
+                    headers = raw.Headers.ToDictionary(x => x.Key, x => x.Value);
             }
+            else if (taskInstance.TriggerDetails is ToastNotification toast)
+            {
+                if (toast.Data?.Values != null)
+                    headers = toast.Data.Values;
+            }
+            else if (taskInstance.TriggerDetails is TileNotification tile)
+            {
+                headers.Add("Tag", tile.Tag);
+            }
+            else
+            {
+                fire = false;
+            }
+
+            if (fire)
+                await this.serviceProvider.RunDelegates<IPushDelegate>(x => x.OnReceived(headers));
 
             deferral.Complete();
         }
