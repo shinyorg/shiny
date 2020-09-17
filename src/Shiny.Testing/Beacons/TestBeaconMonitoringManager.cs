517b10e6a009e7f25a0f6812dd4d685793496c56
Allan Ritchie <allan.ritchie@gmail.com>
2020-09-17 11:30:42 -0400

    Add beacon manager mocks

---
+++ src/Shiny.Testing/Beacons/TestBeaconMonitoringManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Shiny.Beacons;
+
+
+namespace Shiny.Testing.Beacons
+{
+    public class TestBeaconMonitoringManager : IBeaconMonitoringManager
+    {
+        readonly IList<BeaconRegion> regions = new List<BeaconRegion>();
+        public Task<IEnumerable<BeaconRegion>> GetMonitoredRegions()
+            => Task.FromResult<IEnumerable<BeaconRegion>>(this.regions);
+
+
+        public AccessState CurrentAccessState { get; set; } = AccessState.Available;
+        public Task<AccessState> RequestAccess() => Task.FromResult(this.CurrentAccessState);
+
+
+        public Task StartMonitoring(BeaconRegion region)
+        {
+            this.regions.Add(region);
+            return Task.CompletedTask;
+        }
+
+
+        public Task StopAllMonitoring()
+        {
+            this.regions.Clear();
+            return Task.CompletedTask;
+        }
+
+
+        public Task StopMonitoring(string identifier)
+        {
+            var region = this.regions.FirstOrDefault(x => x.Identifier.Equals(identifier, StringComparison.InvariantCultureIgnoreCase));
+            if (region != null)
+                this.regions.Remove(region);
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/Shiny.Testing/Beacons/TestBeaconRangingManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Threading.Tasks;
+using Shiny.Beacons;
+
+
+namespace Shiny.Testing.Beacons
+{
+    public class TestBeaconRangingManager : IBeaconRangingManager
+    {
+        public AccessState CurrentAccessState { get; set; } = AccessState.Available;
+        public Task<AccessState> RequestAccess() => Task.FromResult(this.CurrentAccessState);
+        public Subject<Beacon> BeaconSubject { get; } = new Subject<Beacon>();
+        public IObservable<Beacon> WhenBeaconRanged(BeaconRegion region)
+            => this.BeaconSubject.Where(region.IsBeaconInRegion);
+    }
+}
