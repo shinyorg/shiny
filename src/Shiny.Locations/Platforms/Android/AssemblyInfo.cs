72d1b825125951bdee0634bf1f7ce9ac804ecd4e
Allan Ritchie <allan.ritchie@gmail.com>
2020-09-21 11:57:06 -0400

    Fix location manifest

--- src/Shiny.Locations/Platforms/Android/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-using Android.App;
-
-[assembly: UsesPermission(Android.Manifest.Permission.AccessFineLocation)]
-[assembly: UsesPermission(Android.Manifest.Permission.AccessCoarseLocation)]
-[assembly: UsesPermission(Android.Manifest.Permission.AccessBackgroundLocation)]
-[assembly: UsesPermission("com.google.android.gms.permission.ACTIVITY_RECOGNITION")]
