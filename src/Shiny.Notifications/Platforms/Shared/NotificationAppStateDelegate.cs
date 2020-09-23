39f7f5b351034a9fb50b54815f78bd93de557fd8
Allan Ritchie <allan.ritchie@gmail.com>
2020-09-23 14:57:03 -0400

    prepping appstate delegate onstop along with app shutdown notifications

---
+++ src/Shiny.Notifications/Platforms/Shared/NotificationAppStateDelegate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace Shiny.Notifications
+{
+    public class NotificationAppStateDelegate : ShinyAppStateDelegate
+    {
+        readonly INotificationManager manager;
+        readonly Notification notification;
+
+
+        public NotificationAppStateDelegate(INotificationManager manager, Notification notification)
+        {
+            this.manager = manager; 
+            this.notification = notification;
+        }
+
+
+        public override async void OnStop()
+        {
+            base.OnStop();
+
+            // TODO: this won't be waited - working on it with appstate manager
+            await this.manager.Send(this.notification);
+        }
+    }
+}
