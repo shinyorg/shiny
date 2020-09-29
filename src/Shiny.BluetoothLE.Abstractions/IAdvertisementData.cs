061f63a440475261d29f647b581b33ede3d36177
Allan Ritchie <allan.ritchie@gmail.com>
2020-09-29 15:44:00 -0400

    Add nullability to service uuids

--- src/Shiny.BluetoothLE.Abstractions/IAdvertisementData.cs
+++ src/Shiny.BluetoothLE.Abstractions/IAdvertisementData.cs
@@ -9,7 +9,7 @@ namespace Shiny.BluetoothLE
         bool? IsConnectable { get; }
         AdvertisementServiceData[]? ServiceData { get; }
         ManufacturerData? ManufacturerData { get; }
-        Guid[] ServiceUuids { get; }
+        Guid[]? ServiceUuids { get; }
         int TxPower { get; }
     }
 }
