f30a5d36e6d93480871ff5985132f24a12f3312c
Allan Ritchie <allan.ritchie@gmail.com>
2020-09-30 18:20:49 -0400

    Minor

--- src/Shiny.Testing/Locations/TestMotionActivityManager.cs
+++ src/Shiny.Testing/Locations/TestMotionActivityManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Reactive.Subjects;
 using System.Threading.Tasks;
 using Shiny.Locations;
@@ -20,11 +21,42 @@ namespace Shiny.Testing.Locations
         public Task<IList<MotionActivityEvent>> Query(DateTimeOffset start, DateTimeOffset? end = null)
         {
             end = end ?? DateTimeOffset.UtcNow;
-            var data = this.EventData.Where(x => x.Timestamp >= start && x.Timestamp <= end).ToList();
-            return Task.FromResult<IList<MotionActivityEvent>>(data);
+            lock (this.EventData)
+            { 
+                var data = this.EventData.Where(x => x.Timestamp >= start && x.Timestamp <= end).ToList();
+                return Task.FromResult<IList<MotionActivityEvent>>(data);
+            }
         }
 
 
         public IObservable<MotionActivityEvent> WhenActivityChanged() => this.ActivitySubject;
+
+
+        IDisposable? generating;
+        public bool IsGeneratingTestData => this.generating != null;
+
+
+        public void StartGeneratingTestData(MotionActivityType type, TimeSpan interval)
+            => this.generating = Observable
+                .Interval(interval)
+                .Subscribe(x =>
+                {
+                    var e = new MotionActivityEvent(
+                        type,
+                        MotionActivityConfidence.High,
+                        DateTimeOffset.Now
+                    );
+                    lock (this.EventData)
+                        this.EventData.Add(e);
+                    
+                    this.ActivitySubject.OnNext(e);
+                });
+
+
+        public void StopGeneratingTestData()
+        {
+            this.generating?.Dispose();
+            this.generating = null;
+        }
     }
 }
