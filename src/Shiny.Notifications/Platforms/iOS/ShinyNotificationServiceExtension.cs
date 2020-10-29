b4f6cc8f081cdd6c34ba82550737a44d35b85ef1
Allan Ritchie <allan.ritchie@gmail.com>
2020-10-29 00:51:14 -0400

    Create ShinyNotificationServiceExtension.cs

---
+++ src/Shiny.Notifications/Platforms/iOS/ShinyNotificationServiceExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UserNotifications;
+
+
+namespace Shiny.Notifications
+{
+    public class ShinyNotificationServiceExtension : UNNotificationServiceExtension
+    {
+        public ShinyNotificationServiceExtension(IntPtr handle) : base(handle)
+        {
+        }
+
+
+        public override void DidReceiveNotificationRequest(UNNotificationRequest request, Action<UNNotificationContent> contentHandler)
+        {
+        }
+    }
+}
