e26567d500f07702e1039daf9e89c09a165435e5
Allan Ritchie <allan.ritchie@gmail.com>
2020-11-16 17:03:37 -0500

    Update ILogger.cs

--- src/Shiny.Generators/ILogger.cs
+++ src/Shiny.Generators/ILogger.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 
+using Microsoft.CodeAnalysis;
 
 namespace Shiny.Generators
 {
@@ -10,13 +11,17 @@ namespace Shiny.Generators
         void Error(string msg);
     }
 
-    public class ConsoleLogger : ILogger
+    public class SourceGeneratorLogger : ILogger
     {
+        readonly GeneratorExecutionContext context;
+        public SourceGeneratorLogger(GeneratorExecutionContext context) => this.context = context;
+
         public void Error(string msg) => Write(ConsoleColor.Red, msg);
         public void Info(string msg) => Write(ConsoleColor.White, msg);
         public void Warn(string msg) => Write(ConsoleColor.Yellow, msg);
         void Write(ConsoleColor color, string msg)
         {
+            //this.context.ReportDiagnostic(Diagnostic.Create(new DiagnosticDescriptor("SHINY", msg, msg, "", DiagnosticSeverity.Warning, true), Location.Create(syntaxTree, textSpan), true))
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine(msg);
         }
