87d4fe51a423838f074e0603251bd15fdaa1017a
Allan Ritchie <allan.ritchie@gmail.com>
2020-11-20 16:46:42 -0500

    minor

--- tests/Shiny.Generators.Tests/ShinyCoreGeneratorTests.cs
+++ tests/Shiny.Generators.Tests/ShinyCoreGeneratorTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 
+using Microsoft.CodeAnalysis.CSharp;
 
 namespace Shiny.Generators.Tests
 {
@@ -9,7 +10,69 @@ namespace Shiny.Generators.Tests
         public void Test()
         {
             // https://docs.microsoft.com/en-us/dotnet/api/microsoft.codeanalysis.generatordriver?view=roslyn-dotnet
-            //var driver = new CSharpGeneratorDriver();
+            var driver = CSharpGeneratorDriver.Create(new ShinyCoreGenerator());
+            driver.RunGenerators(CSharpCompilation.Create("Test.dll"));
         }
     }
 }
+/*
+private readonly ITestOutputHelper _output;
+
+	public Tests(ITestOutputHelper output)
+	{
+            _output = output;
+	}
+
+        [Fact]
+        public void Test1()
+        {
+            string source = @"
+namespace Foo
+{
+    class C
+    {
+        void M()
+        {
+        }
+    }
+}";
+            string output = GetGeneratedOutput(source);
+
+            Assert.NotNull(output);
+
+            Assert.Equal("class Foo { }", output);
+        }
+
+        private string GetGeneratedOutput(string source)
+        {
+            var syntaxTree = CSharpSyntaxTree.ParseText(source);
+
+            var references = new List<MetadataReference>();
+            Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            foreach (var assembly in assemblies)
+            {
+                if (!assembly.IsDynamic)
+                {
+                    references.Add(MetadataReference.CreateFromFile(assembly.Location));
+                }
+            }
+
+            var compilation = CSharpCompilation.Create("foo", new SyntaxTree[] { syntaxTree }, references, new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+            // TODO: Uncomment this line if you want to fail tests when the injected program isn't valid _before_ running generators
+            // var compileDiagnostics = compilation.GetDiagnostics();
+            // Assert.False(compileDiagnostics.Any(d => d.Severity == DiagnosticSeverity.Error), "Failed: " + compileDiagnostics.FirstOrDefault()?.GetMessage());
+
+            ISourceGenerator generator = new Generator();
+
+            var driver = CSharpGeneratorDriver.Create(generator);
+            driver.RunGeneratorsAndUpdateCompilation(compilation, out var outputCompilation, out var generateDiagnostics);
+            Assert.False(generateDiagnostics.Any(d => d.Severity == DiagnosticSeverity.Error), "Failed: " + generateDiagnostics.FirstOrDefault()?.GetMessage());
+
+            string output = outputCompilation.SyntaxTrees.Last().ToString();
+
+            _output.WriteLine(output);
+
+            return output;
+        }
+ */
