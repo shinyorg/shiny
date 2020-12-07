aad084af0df2ee5d6aec6802fb94ff4d0b0015db
Allan Ritchie <allan.ritchie@gmail.com>
2020-12-07 13:07:06 -0500

    make channel serializable

--- src/Shiny.Notifications.Abstractions/Channel.cs
+++ src/Shiny.Notifications.Abstractions/Channel.cs
@@ -6,10 +6,7 @@ namespace Shiny.Notifications
 {
     public class Channel
     {
-        public Channel(string identifier) => this.Identifier = identifier;
-
-        public string Identifier { get; }
-        //public string Name { get; }
+        public string Identifier { get; set; }
         public string Description { get; set; }
         public List<ChannelAction> Actions { get; set; } = new List<ChannelAction>();
 
