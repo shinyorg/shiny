b468b0113ae8f5a71e95850799a5ebb1bb68cfa8
Allan Ritchie <allan.ritchie@gmail.com>
2021-02-09 21:42:43 -0500

    Update IPushDelegate.cs

--- src/Shiny.Push/IPushDelegate.cs
+++ src/Shiny.Push/IPushDelegate.cs
@@ -7,8 +7,25 @@ namespace Shiny.Push
 {
     public interface IPushDelegate
     {
+        /// <summary>
+        /// This is fired when the user taps or responds to a notification
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
         Task OnEntry(PushEntryArgs args);
+
+        /// <summary>
+        /// Called when the notification is triggered to the OS
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
         Task OnReceived(IDictionary<string, string> data);
+
+        /// <summary>
+        /// This is called ONLY when the token changes, not during RequestAccess
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
         Task OnTokenChanged(string token);
     }
 }
