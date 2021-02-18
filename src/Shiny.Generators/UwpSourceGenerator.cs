91b29a86ab2abda045a266c7606ca8d12aa72ed0
Allan Ritchie <allan.ritchie@gmail.com>
2021-02-18 12:55:06 -0500

    Notes

--- src/Shiny.Generators/UwpSourceGenerator.cs
+++ src/Shiny.Generators/UwpSourceGenerator.cs
@@ -15,7 +15,26 @@ namespace Shiny.Generators
 
         protected override void Process(IEnumerable<INamedTypeSymbol> osAppTypeSymbols)
         {
-            // nothing to do on UWP at this time - this will gen the startup though
+            var nameSpace = this.Context.Compilation.AssemblyName;
+
+            //// TODO: generate OnActivated
+            //foreach (var app in osAppTypeSymbols)
+            //{
+            //    var builder = new IndentedStringBuilder();
+
+            //    using (builder.BlockInvariant("protected override void OnActivated(global::Windows.ApplicationModel.Activation.IActivatedEventArgs args)"))
+            //    {
+            //        // fired for foreground notifications
+            //        if (args is ToastNotificationActivatedEventArgs not)
+            //        {
+            //            var args1 = not.Argument;
+            //            // TODO: Handle activation according to argument
+            //        }
+
+            //        Window.Current.Activate();
+            //    }
+            //    this.Context.AddSource("", builder.ToString());
+            //}
         }
     }
 }
