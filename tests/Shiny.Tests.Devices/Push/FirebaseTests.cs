83bcc900e39737148e5aa705bcb02a43c0189b26
Allan Ritchie <allan.ritchie@gmail.com>
2021-03-18 00:17:00 -0400

    getting more tests ready

--- tests/Shiny.Tests.Devices/Push/FirebaseTests.cs
+++ tests/Shiny.Tests.Devices/Push/FirebaseTests.cs
@@ -1,12 +1,25 @@
 ﻿using System;
-
+using Shiny.Push;
 
 namespace Shiny.Tests.Devices.Push
 {
     public class FirebaseTests
     {
+        readonly Shiny.Push.FirebaseMessaging.PushManager pushManager;
+
+
         public FirebaseTests()
         {
+            ShinyHost.Init(TestStartup.CurrentPlatform, new ActionStartup
+            {
+                BuildServices = (services) =>
+                {
+                    services.UseFirebaseMessaging<TestPushDelegate>();
+                }
+            });
+
+            this.pushManager = (Shiny.Push.FirebaseMessaging.PushManager)ShinyHost.Resolve<IPushManager>();
+            //new FirebaseMessagingService();
         }
     }
 }
--- tests/Shiny.Tests.Devices/Push/TestPushDelegate.cs
+++ tests/Shiny.Tests.Devices/Push/TestPushDelegate.cs
@@ -8,8 +8,27 @@ namespace Shiny.Tests.Devices.Push
 {
     public class TestPushDelegate : IPushDelegate
     {
-        public Task OnEntry(PushEntryArgs args) => throw new NotImplementedException();
-        public Task OnReceived(IDictionary<string, string> data) => throw new NotImplementedException();
-        public Task OnTokenChanged(string token) => throw new NotImplementedException();
-    }
+        public Action<PushEntryArgs>? Entry { get; set; }
+        public Task OnEntry(PushEntryArgs args)
+        {
+            this.Entry?.Invoke(args);
+            return Task.CompletedTask;
+        }
+
+
+        public Action<IDictionary<string, string>>? Received { get; set; }
+        public Task OnReceived(IDictionary<string, string> data)
+        {
+            this.Received?.Invoke(data);
+            return Task.CompletedTask;
+        }
+
+
+        public Action<string>? TokenChanged { get; set; }
+        public Task OnTokenChanged(string token)
+        {
+            this.TokenChanged?.Invoke(token);
+            return Task.CompletedTask;
+        }
+}
 }
