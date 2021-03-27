97cce61719d58b47da8edfa71d85927712921c6f
Allan Ritchie <allan.ritchie@gmail.com>
2021-03-27 11:58:47 -0400

    Update IPushDelegate.cs

--- src/Shiny.Push/IPushDelegate.cs
+++ src/Shiny.Push/IPushDelegate.cs
@@ -9,13 +9,20 @@ namespace Shiny.Push
     public interface IPushDelegate
     {
         /// <summary>
-        /// Called when the notification is triggered to the OS
+        /// This is called when the user taps/responds to a push notification
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="response"></param>
+        /// <returns></returns>
+        Task OnEntry(IDictionary<string, string> data, NotificationResponse response);
+
+        /// <summary>
+        /// Called when a push is received. BACKGROUND NOTE: if your app is in the background, you need to pass data parameters (iOS: content-available:1) to get this to fire
         /// </summary>
         /// <param name="data">This is the data portion of the push notification</param>
         /// <param name="notification">The notification content - this can be null on iOS when received in the background or if a body was not sent</param>
-        /// <param name="entry">if the user has tapped on an notification, this will true otherwise false</param>
         /// <returns></returns>
-        Task OnAction(IDictionary<string, string> data, Notification? notification, bool entry);
+        Task OnReceived(IDictionary<string, string> data, Notification? notification);
 
 
         /// <summary>
