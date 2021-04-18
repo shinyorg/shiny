81d40347b052d0f04890171b2783fb70e00865c8
Allan Ritchie <allan.ritchie@gmail.com>
2021-04-17 23:50:50 -0400

    sample updates

--- samples/Samples.iOS/AppDelegate.cs
+++ samples/Samples.iOS/AppDelegate.cs
@@ -1,34 +1,25 @@
-﻿using System.Reactive.Linq;
-using Foundation;
+﻿using Foundation;
 using Shiny;
-using Shiny.Notifications;
 using UIKit;
 using Samples.Infrastructure;
-using UserNotifications;
 
-
-[assembly: Shiny.ShinyApplication(
+[assembly: ShinyApplication(
     ShinyStartupTypeName = "Samples.SampleStartup",
     XamarinFormsAppTypeName = "Samples.App"
 )]
 namespace Samples.iOS
 {
-    [Foundation.Register("AppDelegate")]
+    [Register("AppDelegate")]
     public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
     {
         partial void OnPostFinishedLaunching(UIApplication app, NSDictionary options)
         {
             // for the people who don't know how iOS notifications work in the foreground!
-            var dialogs = ShinyHost.Resolve<IDialogs>();
-            ShinyHost
-                .Resolve<iOSNotificationDelegate>()
-                .WhenPresented()
-                .Where(_ => app.ApplicationState == UIApplicationState.Active)
-                .SubOnMainThread(async x =>
-                {
-                    await dialogs.Snackbar(x.Notification.Request.Content.Body);
-                    x.CompletionHandler.Invoke(UNNotificationPresentationOptions.Alert);
-                });
+            ShinyHost.Resolve<AppleLifecycle>().RegisterForNotificationPresentation(async notification =>
+            {
+                if (app.ApplicationState == UIApplicationState.Active)
+                    await ShinyHost.Resolve<IDialogs>().Snackbar(notification.Request?.Content?.Body ?? "NO CONTENT");
+            });
         }
     }
 }
