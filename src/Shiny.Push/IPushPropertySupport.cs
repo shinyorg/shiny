786acd3d3bdb2e2383012222b99edae38b043061
Allan Ritchie <allan.ritchie@gmail.com>
2021-04-30 16:18:01 -0400

    Update IPushPropertySupport.cs

--- src/Shiny.Push/IPushPropertySupport.cs
+++ src/Shiny.Push/IPushPropertySupport.cs
@@ -6,11 +6,6 @@ namespace Shiny.Push
 {
     public interface IPushPropertySupport
     {
-        /// <summary>
-        /// The list of available properties, if null, any property is supported
-        /// </summary>
-        string[]? SupportedProperties { get; }
-
         IReadOnlyDictionary<string, string> CurrentProperties { get; }
 
         void ClearProperties();
