d5098e37dce3f96be52ede0defc6e52cce0c3951
Allan Ritchie <allan.ritchie@gmail.com>
2021-06-30 13:08:40 -0400

    sample cleanup

--- samples/Samples/AccessViewModel.cs
+++ samples/Samples/AccessViewModel.cs
@@ -13,13 +13,18 @@ using Shiny.Notifications;
 using Shiny.SpeechRecognition;
 using Shiny.Push;
 using Shiny.Nfc;
+using Samples.Infrastructure;
 
 
 namespace Samples
 {
     public class AccessViewModel : ViewModel
     {
+        readonly IDialogs dialogs;
+
+
         public AccessViewModel(IJobManager jobs,
+                               IDialogs dialogs,
                                INotificationManager? notifications = null,
                                ISpeechRecognizer? speech = null,
                                IGeofenceManager? geofences = null,
@@ -31,62 +36,56 @@ namespace Samples
                                IPushManager? push = null,
                                INfcManager? nfc = null)
         {
-            this.Append("Jobs", AccessState.Unknown, () => jobs.RequestAccess());
+            this.dialogs = dialogs;
 
-            if (notifications != null)
-                this.Append("Notifications", AccessState.Unknown, () => notifications.RequestAccess());
-
-            if (speech != null)
-                this.Append("Speech", AccessState.Unknown, () => speech.RequestAccess());
-
-            if (activityManager != null)
-                this.Append("Motion Activity", AccessState.Unknown, () => activityManager.RequestAccess());
-
-            if (gps != null)
+            this.Append("Jobs", jobs, () => jobs.RequestAccess());
+            this.Append("Notifications", notifications, () => notifications!.RequestAccess());
+            this.Append("Speech", speech, () => speech!.RequestAccess());
+            this.Append("Motion Activity", activityManager, () => activityManager!.RequestAccess());
+            this.Append("GPS (Background)", gps, () => gps!.RequestAccess(GpsRequest.Realtime(true)));
+            this.Append("Geofences", geofences, () => geofences!.RequestAccess());
+            this.Append("BluetoothLE Central", bluetooth, () => bluetooth!.RequestAccess().ToTask(CancellationToken.None));
+            this.Append("iBeacons (Ranging)", beaconRanging, () => beaconRanging!.RequestAccess());
+            this.Append("iBeacons (Monitoring)", beaconMonitoring, () => beaconMonitoring!.RequestAccess());
+            this.Append("Push", push, async () =>
             {
-                var request = new GpsRequest { UseBackground = true };
-                this.Append("GPS (Background)", gps.GetCurrentStatus(request), () => gps.RequestAccess(request));
-            }
-
-            if (geofences != null)
-                this.Append("Geofences", geofences.Status, () => geofences.RequestAccess());
-
-            if (bluetooth != null)
-                this.Append("BluetoothLE Central", bluetooth.Status, () => bluetooth.RequestAccess().ToTask(CancellationToken.None));
-
-            if (beaconRanging != null)
-                this.Append("iBeacons (Ranging)", AccessState.Unknown, () => beaconRanging.RequestAccess());
-
-            if (beaconMonitoring != null)
-                this.Append("iBeacons (Monitoring)", AccessState.Unknown, () => beaconMonitoring.RequestAccess());
-
-            if (push != null)
-                this.Append("Push", AccessState.Unknown, async () =>
-                {
-                    var status = await push.RequestAccess();
-                    return status.Status;
-                });
-
-            if (nfc != null)
-                this.Append("NFC", AccessState.Unknown, () => nfc.RequestAccess().ToTask(CancellationToken.None));
+                var status = await push!.RequestAccess();
+                return status.Status;
+            });
+            this.Append("NFC", nfc, () => nfc!.RequestAccess().ToTask(CancellationToken.None));
         }
 
 
         public List<CommandItem> List { get; } = new List<CommandItem>();
 
 
-        void Append(string text, AccessState current, Func<Task<AccessState>> request)
+        void Append(string text, object? service, Func<Task<AccessState>> request)
         {
-            var item = new CommandItem
+            CommandItem item;
+
+            if (service == null)
             {
-                Text = text,
-                Detail = current.ToString()
-            };
-            item.PrimaryCommand = ReactiveCommand.CreateFromTask(async () =>
+                item = new CommandItem
+                {
+                    Text = text,
+                    PrimaryCommand = ReactiveCommand.CreateFromTask(()
+                        => this.dialogs.Snackbar($"{text} not available on this platform or is not registered")
+                    )
+                };
+            }
+            else
             {
-                var r = await request();
-                item.Detail = r.ToString();
-            });
+                item = new CommandItem
+                {
+                    Text = text,
+                    Detail = AccessState.Unknown.ToString()
+                };
+                item.PrimaryCommand = ReactiveCommand.CreateFromTask(async () =>
+                {
+                    var r = await request();
+                    item.Detail = r.ToString();
+                });
+            }
             this.List.Add(item);
         }
     }
