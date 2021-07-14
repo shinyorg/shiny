dfc8197f6275a41502dec67889c4b8c27e93a2df
Allan Ritchie <allan.ritchie@gmail.com>
2021-07-14 14:44:12 -0400

    Update MainViewModel.cs

--- samples/Samples/MainViewModel.cs
+++ samples/Samples/MainViewModel.cs
@@ -3,25 +3,18 @@ using System.Windows.Input;
 using Prism.Navigation;
 using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
-using Samples.Notifications;
 
 
 namespace Samples
 {
     public class MainViewModel : ViewModel
     {
-        readonly INavigationService navigator;
-        readonly NotificationDelegate? notificationDelegate;
-
-
-        public MainViewModel(INavigationService navigator, NotificationDelegate? notificationDelegate = null)
+        public MainViewModel(INavigationService navigator)
         {
-            this.navigator = navigator;
-            this.notificationDelegate = notificationDelegate;
             this.Navigate = ReactiveCommand.CreateFromTask<string>(async arg =>
             {
                 this.IsPresented = false;
-                await this.navigator.Navigate(arg);
+                await navigator.Navigate(arg);
             });
         }
 
