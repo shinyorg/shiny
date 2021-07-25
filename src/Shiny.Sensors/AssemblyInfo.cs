42fdc59aa34570c1888a1bfe2a36ac3fedb4b9d9
Allan Ritchie <allan.ritchie@gmail.com>
2021-07-25 17:04:55 -0400

    remove heart rate sensor

--- src/Shiny.Sensors/AssemblyInfo.cs
+++ src/Shiny.Sensors/AssemblyInfo.cs
@@ -7,7 +7,6 @@
 [assembly: StaticGeneration("Shiny.Sensors.IBarometer", "ShinyBarometer")]
 [assembly: StaticGeneration("Shiny.Sensors.ICompass", "ShinyCompass")]
 [assembly: StaticGeneration("Shiny.Sensors.IGyroscope", "ShinyGyroscope")]
-[assembly: StaticGeneration("Shiny.Sensors.IHeartRateMonitor", "ShinyHeartRate")]
 [assembly: StaticGeneration("Shiny.Sensors.IHumidity", "ShinyHumidity")]
 [assembly: StaticGeneration("Shiny.Sensors.IMagnetometer", "ShinyMagnetometer")]
 [assembly: StaticGeneration("Shiny.Sensors.IPedometer", "ShinyPedometer")]
