dccaec5cd114b84e8aebf1fabe2bb4ee9737186b
Allan Ritchie <allan.ritchie@gmail.com>
2021-09-10 21:42:12 -0400

    Minor

--- src/Shiny.Push.AzureNotificationHubs/Platforms/Shared/PushManager.cs
+++ src/Shiny.Push.AzureNotificationHubs/Platforms/Shared/PushManager.cs
@@ -16,12 +16,11 @@ namespace Shiny.Push.AzureNotificationHubs
                                IPushTagSupport,
                                IShinyStartupTask
     {
-        const string EXPIRY_DATE = nameof(EXPIRY_DATE);
         readonly INativeAdapter native;
         readonly ILogger logger;
         readonly PushContainer container;
         readonly AzureNotificationConfig config;
-        // TODO: watch push channel expiration?
+
 
         public PushManager(INativeAdapter native,
                            ILogger<PushManager> logger,
@@ -64,13 +63,6 @@ namespace Shiny.Push.AzureNotificationHubs
         public string? CurrentRegistrationToken => this.InstallationId;
         public DateTime? CurrentRegistrationTokenDate => this.container.CurrentRegistrationTokenDate;
         public string[]? RegisteredTags => this.container.RegisteredTags;
-        public DateTimeOffset? ExpiryDate
-        {
-            get => this.container.Store.Get<DateTimeOffset?>(EXPIRY_DATE);
-            private set => this.container.Store.SetOrRemove(nameof(this.ExpiryDate), value);
-        }
-
-
         public string? InstallationId
         {
             get => this.container.Store.Get<string>(nameof(this.InstallationId));
