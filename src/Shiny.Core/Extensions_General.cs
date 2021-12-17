e04f1f4e4ebb43fc147ceac1f163d7fe26b1d696
Allan Ritchie <allan.ritchie@gmail.com>
2021-12-17 10:44:09 -0500

    More slight namespace alignments

--- src/Shiny.Core/Extensions_General.cs
+++ src/Shiny.Core/Extensions_General.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 
 
 namespace Shiny
 {
     public static class GeneralExtensions
     {
+        public static PropertyInfo GetPropertyInfo<TSender, TRet>(this TSender sender, Expression<Func<TSender, TRet>> expression)
+        {
+            if (sender == null)
+                throw new ArgumentException("Sender is null");
+
+            var member = (expression as LambdaExpression)?.Body as MemberExpression;
+            if (member == null)
+                throw new ArgumentException("Invalid lamba expression - body is not a member expression");
+
+            var property = sender.GetType().GetRuntimeProperty(member.Member.Name);
+            return property;
+        }
+
+
+        public static object? GetDefaultValue(this Type t)
+        {
+            if (t.IsValueType && Nullable.GetUnderlyingType(t) == null)
+                return Activator.CreateInstance(t);
+
+            return null;
+        }
+
         /// <summary>
         /// Extension method to String.IsNullOrWhiteSpace
         /// </summary>
