fe7917b355dd70cd89b2ad108ed02fae8967437a
Allan Ritchie <allan.ritchie@gmail.com>
2022-03-09 21:38:17 -0500

    WIP on NFC

---
+++ src/Shiny.Nfc/Platforms/iOS/NfcDelegates.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CoreNFC;
+using Foundation;
+
+
+namespace Shiny.Nfc
+{
+    public class NfcDelegates : NFCNdefReaderSessionDelegate, INFCTagReaderSessionDelegate
+    {
+        public Action<NFCNdefMessage[]>? OnDidDetect { get; set; }
+        public Action<NSError>? OnDidInvalidate { get; set; }
+
+        public override void DidDetect(NFCNdefReaderSession session, NFCNdefMessage[] messages) => this.OnDidDetect?.Invoke(messages);
+        public void DidInvalidate(NFCTagReaderSession session, NSError error) => this.OnDidInvalidate?.Invoke(error);
+        public override void DidInvalidate(NFCNdefReaderSession session, NSError error) => this.OnDidInvalidate?.Invoke(error);
+    }
+}
--- src/Shiny.Nfc/Platforms/iOS/NfcManager.cs
+++ src/Shiny.Nfc/Platforms/iOS/NfcManager.cs
@@ -7,15 +7,18 @@ using CoreFoundation;
 using CoreNFC;
 using Foundation;
 
+using UIKit;
 
 namespace Shiny.Nfc
 {
+    //https://betterprogramming.pub/working-with-nfc-tags-in-ios-13-d08c7d183981
     //https://docs.microsoft.com/en-us/xamarin/ios/platform/introduction-to-ios11/corenfc
-    public class NfcManager : NFCNdefReaderSessionDelegate, INfcManager, IShinyStartupTask
+    public class NfcManager : INfcManager, IShinyStartupTask
     {
         readonly Subject<NDefRecord[]> recordSubj = new Subject<NDefRecord[]>();
         readonly Subject<NSError> invalidSubj = new Subject<NSError>();
         readonly AppleLifecycle lifecycle;
+
 
         public NfcManager(AppleLifecycle lifecycle) => this.lifecycle = lifecycle;
 
@@ -29,37 +32,7 @@ namespace Shiny.Nfc
             });
         }
 
-        public override void DidDetect(NFCNdefReaderSession session, NFCNdefMessage[] messages)
-        {
-            foreach (var message in messages)
-            {
-                var list = new List<NDefRecord>(message.Records.Length);
-                foreach (var record in message.Records)
-                {
-                    list.Add(new NDefRecord
-                    {
-                        Identifier = record.Identifier.ToArray(),
-                        Payload = record.Payload?.ToArray(),
-                        Uri = record.WellKnownTypeUriPayload?.ToString(),
-                        PayloadType = record.TypeNameFormat switch
-                        {
-                            NFCTypeNameFormat.AbsoluteUri => NfcPayloadType.Uri,
-                            NFCTypeNameFormat.Empty => NfcPayloadType.Empty,
-                            NFCTypeNameFormat.NFCExternal => NfcPayloadType.External,
-                            NFCTypeNameFormat.NFCWellKnown => NfcPayloadType.WellKnown,
-                            NFCTypeNameFormat.Unchanged => NfcPayloadType.Unchanged,
-                            //case NFCTypeNameFormat.Media: // TODO: mime?
-                            _ => NfcPayloadType.Unknown
-                        }
-                    });
-                }
-                this.recordSubj.OnNext(list.ToArray());
-            }
-        }
 
-
-        public override void DidInvalidate(NFCNdefReaderSession session, NSError error)
-            => Task.Run(() => this.invalidSubj.OnNext(error));
 
 
         public IObservable<AccessState> RequestAccess()
@@ -73,50 +46,65 @@ namespace Shiny.Nfc
         }
 
 
+
+        public IObservable<object> Publish() => Observable.Create<object>(ob => 
+        {
+            if (!UIDevice.CurrentDevice.CheckSystemVersion(13, 0))
+                throw new InvalidOperationException("iOS 13+ is required");
+
+            var myDelegate = new NfcDelegates();
+            var session = new NFCTagReaderSession(
+                NFCPollingOption.Iso14443 | NFCPollingOption.Iso15693 | NFCPollingOption.Iso18092, 
+                myDelegate, 
+                DispatchQueue.CurrentQueue
+            );
+            session.BeginSession();
+
+            // TODO: detect a tag, write back to it, allow user to decide writeback at that point or force them to pass tag writeback with arg?
+            return () => 
+            {
+                session.InvalidateSession();
+                session.Dispose();
+            };
+        });
+
         public IObservable<NDefRecord[]> SingleRead() => this.DoRead(true);
         public IObservable<NDefRecord[]> ContinuousRead() => this.DoRead(false);
 
 
         protected virtual IObservable<NDefRecord[]> DoRead(bool singleRead) => Observable.Create<NDefRecord[]>(ob =>
         {
-            var session = new NFCNdefReaderSession(this, DispatchQueue.CurrentQueue, true);
+            var myDelegate = new NfcDelegates();
+            var session = new NFCNdefReaderSession(myDelegate, DispatchQueue.CurrentQueue, true);
             var cancel = false;
-
-            var invalidSub = this.invalidSubj.Subscribe(x =>
+            
+            myDelegate.OnDidDetect = messages =>
             {
-                switch (x.Code)
-                {
-                    case 200:
-                        ob.OnCompleted();
-                        break;
-
-                    case 203:
-                        if (!cancel)
+                foreach (var message in messages)
+                { 
+                    var list = new List<NDefRecord>(message.Records.Length);
+                    foreach (var record in message.Records)
+                    {
+                        list.Add(new NDefRecord
                         {
-                            Task.Delay(500).ContinueWith(_ =>
+                            Identifier = record.Identifier.ToArray(),
+                            Payload = record.Payload?.ToArray(),
+                            Uri = record.WellKnownTypeUriPayload?.ToString(),
+                            PayloadType = record.TypeNameFormat switch
                             {
-                                if (!cancel)
-                                {
-                                    session = new NFCNdefReaderSession(this, DispatchQueue.CurrentQueue, true);
-                                    session.BeginSession();
-                                }
-                            });
-                        }
-                        break;
-
-                    case 204:
-                        break;
-
-                    default:
-                        ob.OnError(new Exception(x.LocalizedDescription.ToString()));
-                        break;
+                                NFCTypeNameFormat.AbsoluteUri => NfcPayloadType.Uri,
+                                NFCTypeNameFormat.Empty => NfcPayloadType.Empty,
+                                NFCTypeNameFormat.NFCExternal => NfcPayloadType.External,
+                                NFCTypeNameFormat.NFCWellKnown => NfcPayloadType.WellKnown,
+                                NFCTypeNameFormat.Unchanged => NfcPayloadType.Unchanged,
+                                //case NFCTypeNameFormat.Media: // TODO: mime?
+                                _ => NfcPayloadType.Unknown
+                            }
+                        });
+                    }
+                    ob.OnNext(list.ToArray());
                 }
-            });
 
-            session.BeginSession(); // begin initial session
-            var sub = this.recordSubj.Subscribe(x =>
-            {
-                ob.OnNext(x);
                 if (singleRead)
                 {
                     ob.OnCompleted();
@@ -126,17 +114,52 @@ namespace Shiny.Nfc
                     session.InvalidateSession();
 
                     // begin new session
-                    session = new NFCNdefReaderSession(this, DispatchQueue.CurrentQueue, true);
+                    session = new NFCNdefReaderSession(myDelegate, DispatchQueue.CurrentQueue, true);
                     session.BeginSession();
                 }
-            });
+            };
+
+            myDelegate.OnDidInvalidate = e =>
+            {
+                var error = (NFCReaderError)(long)e.Code;
+
+                switch (error)
+                {
+                    case NFCReaderError.ReaderSessionInvalidationErrorUserCanceled:
+                        ob.OnCompleted();
+                        // watch for null here
+                        break;
+
+                    case NFCReaderError.ReaderSessionInvalidationErrorSystemIsBusy:
+                        if (!cancel)
+                        {
+                            Task.Delay(500).ContinueWith(_ =>
+                            {
+                                if (!cancel)
+                                {
+                                    session = new NFCNdefReaderSession(myDelegate, DispatchQueue.CurrentQueue, true);
+                                    session.BeginSession();
+                                }
+                            });
+                        }
+                        break;
+
+                    case NFCReaderError.ReaderSessionInvalidationErrorFirstNDEFTagRead:
+                        break;
+
+                    default:
+                        ob.OnError(new Exception(e.LocalizedDescription.ToString()));
+                        break;
+                }
+            };
+
+            session.BeginSession(); // begin initial session
+
             return () =>
             {
                 cancel = true;
                 session?.InvalidateSession();
-                sub.Dispose();
-                invalidSub.Dispose();
             };
-        });
+        });        
     }
 }
