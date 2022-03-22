1a6b14c65aa56bf207d66d1b346243ebecba8905
Allan Ritchie <allan.ritchie@gmail.com>
2022-03-22 14:45:31 -0400

    Update Position.cs

--- src/Shiny.Core/Locations/Position.cs
+++ src/Shiny.Core/Locations/Position.cs
@@ -20,20 +20,7 @@ namespace Shiny.Locations
 
         public double Latitude { get; }
         public double Longitude { get; }
-
-
-        public Distance GetDistanceTo(Position other)
-        {
-            var d1 = this.Latitude * (Math.PI / 180.0);
-            var num1 = this.Longitude * (Math.PI / 180.0);
-            var d2 = other.Latitude * (Math.PI / 180.0);
-            var num2 = other.Longitude * (Math.PI / 180.0) - num1;
-            var d3 = Math.Pow(Math.Sin((d2 - d1) / 2.0), 2.0) +
-                     Math.Cos(d1) * Math.Cos(d2) * Math.Pow(Math.Sin(num2 / 2.0), 2.0);
-
-            var meters = 6376500.0 * (2.0 * Math.Atan2(Math.Sqrt(d3), Math.Sqrt(1.0 - d3)));
-            return Distance.FromMeters(meters);
-        }
+        public Distance GetDistanceTo(Position other) => Distance.Between(this, other);
 
 
         //https://stackoverflow.com/questions/2042599/direction-between-2-latitude-longitude-points-in-c-sharp
