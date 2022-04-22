5aed3241fa9c45a7c4686ddd84080ab0fef6e9b9
Allan Ritchie <allan.ritchie@gmail.com>
2022-04-22 12:01:05 -0400

    Finish removing docs

--- build/Tasks/DefaultTask.cs
+++ build/Tasks/DefaultTask.cs
@@ -7,7 +7,6 @@ namespace ShinyBuild.Tasks
     [TaskName("Default")]
     [IsDependentOn(typeof(CopyArtifactsTask))]
     [IsDependentOn(typeof(NugetDeployTask))]
-    [IsDependentOn(typeof(DocTask))]
     [IsDependentOn(typeof(ReleaseAnnouncementTask))]
     [IsDependentOn(typeof(GitHubReleaseTask))]
     public sealed class DefaultTarget : FrostingTask<BuildContext> { }
