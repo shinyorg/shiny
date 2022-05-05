b395468a18605720ddfaca40fe15fa2c245dcf8c
Allan Ritchie <allan.ritchie@gmail.com>
2022-05-05 15:35:19 -0400

    Clean out some deadwood and fix up frameworks - this is the beginning of the net6 enablement

---
+++ src/Shiny.Extensions.Configuration/Platforms/Android/ConfigurationBuilderExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Android.App;
+using Shiny.Extensions.Configuration;
+using System;
+using System.Linq;
+
+
+namespace Microsoft.Extensions.Configuration
+{
+    public static partial class ConfigurationBuilderExtensions
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="fileName"></param>
+        /// <param name="optional"></param>
+        /// <returns></returns>
+        public static IConfigurationBuilder AddJsonAndroidAsset(this IConfigurationBuilder builder, string fileName = "appsettings.json", bool optional = true)
+        {
+            var assets = Application.Context.Assets;
+            var exists = assets
+                .List("")
+                .Any(x => x.Equals(fileName, StringComparison.InvariantCultureIgnoreCase));
+
+            if (exists)
+                builder.AddJsonStream(assets.Open(fileName));
+
+            else if (!optional)
+                throw new InvalidOperationException($"Android Asset file '{fileName}' not found");
+
+            return builder;
+        }
+
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <returns></returns>
+        public static IConfigurationBuilder AddAndroidPreferences(this IConfigurationBuilder builder)
+            => builder.Add(new SharedPreferencesConfigurationSource());
+    }
+}
---
+++ src/Shiny.Extensions.Configuration/Platforms/iOS/NSUserDefaultsConfigurationSource.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Configuration;
+
+
+namespace Shiny.Extensions.Configuration
+{
+    public class NSUserDefaultsConfigurationSource : IConfigurationSource
+    {
+        public IConfigurationProvider Build(IConfigurationBuilder builder)
+            => new NSUserDefaultsConfigurationProvider();
+    }
+}
