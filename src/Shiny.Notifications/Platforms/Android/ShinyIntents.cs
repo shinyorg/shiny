6c92fc739a1101e822c4e6b25d19640589115585
Allan Ritchie <allan.ritchie@gmail.com>
2022-05-24 21:30:09 -0400

    Delete ShinyIntents.cs

--- src/Shiny.Notifications/Platforms/Android/ShinyIntents.cs
+++
@@ -1,7 +0,0 @@
-﻿//namespace Shiny.Notifications
-//{
-//    public static class ShinyIntents
-//    {
-//        public const string NotificationClickAction = "SHINY_LOCAL_NOTIFICATION_CLICK";
-//    }
-//}
