e2557983553d8727b38db85930c41563a9031886
Allan Ritchie <allan.ritchie@gmail.com>
2022-06-07 15:49:42 -0400

    Notifications WIP

--- src/Shiny.Notifications/Infrastructure/NotificationStoreConverter.cs
+++ src/Shiny.Notifications/Infrastructure/NotificationStoreConverter.cs
@@ -6,6 +6,50 @@ namespace Shiny.Notifications.Infrastructure;
 
 public class NotificationStoreConverter : IStoreConverter<Notification>
 {
-    public Notification FromStore(IDictionary<string, object> values) => throw new System.NotImplementedException();
-    public IEnumerable<(string Property, object value)> ToStore(Notification entity) => throw new System.NotImplementedException();
+    public Notification FromStore(IDictionary<string, object> values)
+    {
+        var result = new Notification
+        {
+            Id = (int)values["Identifier"],
+            Message = (string)values[nameof(Notification.Message)],
+            Channel = (string)values[nameof(Notification.Channel)],
+            BadgeCount = (int)values[nameof(Notification.BadgeCount)]
+        };
+        
+        if (values.ContainsKey(nameof(Notification.Title)))
+            result.Title = (string)values[nameof(Notification.Title)];
+
+        if (values.ContainsKey(nameof(Notification.Thread)))
+            result.Thread = (string)values[nameof(Notification.Thread)];
+
+        if (values.ContainsKey(nameof(Notification.ImageUri)))
+            result.ImageUri = (string)values[nameof(Notification.ImageUri)];
+
+        //if (values.Contains(nameof(Notification.Geofence)))
+        //if (values.Contains(nameof(Notification.RepeatInterval)))
+        return result;
+    }
+
+
+    public IEnumerable<(string Property, object value)> ToStore(Notification entity)
+    {
+        yield return (nameof(entity.Identifier), entity.Id.ToString());
+        yield return (nameof(entity.Channel), entity.Channel!);
+        yield return (nameof(entity.Message), entity.Message!);
+
+        if (entity.Title != null)
+            yield return (nameof(entity.Title), entity.Title);
+
+        if (entity.Thread != null)
+            yield return (nameof(entity.Thread), entity.Thread);
+
+        if (entity.ImageUri != null)
+            yield return (nameof(entity.ImageUri), entity.ImageUri);
+
+        // I don't really want this here - maybe consider platform interceptors of some sort
+#if ANDROID
+        //result.Android
+#endif
+
+    }
 }
