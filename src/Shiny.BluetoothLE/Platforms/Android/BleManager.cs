d617ae3914b304a550435e1ef671b3e931d100f0
Allan Ritchie <allan.ritchie@gmail.com>
2022-07-18 23:34:17 -0400

    Minor

--- src/Shiny.BluetoothLE/Platforms/Android/BleManager.cs
+++ src/Shiny.BluetoothLE/Platforms/Android/BleManager.cs
@@ -7,122 +7,121 @@ using Shiny.BluetoothLE.Internals;
 using Android;
 using Android.Bluetooth;
 
+namespace Shiny.BluetoothLE;
 
-namespace Shiny.BluetoothLE
+
+public class BleManager : AbstractBleManager,
+                          ICanControlAdapterState,
+                          ICanViewPairedPeripherals
 {
-    public class BleManager : AbstractBleManager,
-                              ICanControlAdapterState,
-                              ICanViewPairedPeripherals
+    public const string BroadcastReceiverName = "com.shiny.bluetoothle.ShinyBleCentralBroadcastReceiver";
+    readonly ManagerContext context;
+
+
+    public BleManager(ManagerContext context) => this.context = context;
+
+
+    bool isScanning;
+    public override bool IsScanning => this.isScanning;
+
+
+    public override IObservable<IPeripheral?> GetKnownPeripheral(string peripheralUuid)
     {
-        public const string BroadcastReceiverName = "com.shiny.bluetoothle.ShinyBleCentralBroadcastReceiver";
-        readonly ManagerContext context;
+        var address = Guid
+            .Parse(peripheralUuid)
+            .ToByteArray()
+            .Skip(10)
+            .Take(6)
+            .ToArray();
+
+        var native = this.context.Manager.Adapter!.GetRemoteDevice(address);
+        if (native == null)
+            return Observable.Return<IPeripheral?>(null);
+
+        var peripheral = this.context.GetDevice(native);
+        return Observable.Return(peripheral);
+    }
 
 
-        public BleManager(ManagerContext context) => this.context = context;
-
-
-        bool isScanning;
-        public override bool IsScanning => this.isScanning;
-
-
-        public override IObservable<IPeripheral?> GetKnownPeripheral(string peripheralUuid)
-        {
-            var address = Guid
-                .Parse(peripheralUuid)
-                .ToByteArray()
-                .Skip(10)
-                .Take(6)
-                .ToArray();
-
-            var native = this.context.Manager.Adapter!.GetRemoteDevice(address);
-            if (native == null)
-                return Observable.Return<IPeripheral?>(null);
-
-            var peripheral = this.context.GetDevice(native);
-            return Observable.Return(peripheral);
-        }
-
-
-        public override IObservable<IEnumerable<IPeripheral>> GetConnectedPeripherals(string? serviceUuid = null)
-            => Observable.Return(this.context
-                .Manager
-                .GetConnectedDevices(ProfileType.Gatt)
-                .Where(x => x.Type == BluetoothDeviceType.Dual || x.Type == BluetoothDeviceType.Le) // just in case
-                .Select(this.context.GetDevice)
-            );
-
-
-        static readonly AndroidPermission[] permissions = new []
-        {
-            new AndroidPermission(Manifest.Permission.Bluetooth, null, null),
-            new AndroidPermission(Manifest.Permission.BluetoothAdmin, null, null),
-            new AndroidPermission(Manifest.Permission.BluetoothScan, 31, null),
-            new AndroidPermission(Manifest.Permission.BluetoothConnect, 31, null),
-            new AndroidPermission(Manifest.Permission.AccessFineLocation, null, 31)
-        };
-        public override IObservable<AccessState> RequestAccess() => Observable.FromAsync(async ct =>
-        {
-            if (!this.context.Android.EnsureAllManifestEntries(permissions))
-                return AccessState.NotSetup;
-
-            var results = await this.context
-                .Android
-                .RequestFilteredPermissions(permissions)
-                .ToTask(ct)
-                .ConfigureAwait(false);
-
-            return results.IsSuccess()
-                ? this.context.Status // now look at the actual device state
-                : AccessState.Denied;
-        });
-
-
-        public override IObservable<ScanResult> Scan(ScanConfig? config = null)
-        {
-            if (this.IsScanning)
-                throw new ArgumentException("There is already an active scan");
-
-            return this
-                .RequestAccess()
-                .Do(access =>
-                {
-                    if (access != AccessState.Available)
-                        throw new PermissionException("BluetoothLE", access);
-
-                    this.IsScanning = true;
-                })
-                .SelectMany(_ => this.context.Scan(config ?? new ScanConfig()))
-                .Finally(() => this.IsScanning = false);
-        }
-
-
-        public override void StopScan()
-        {
-            this.context.StopScan();
-            this.isScanning = false;
-        }
-
-
-        public IObservable<IEnumerable<IPeripheral>> GetPairedPeripherals() => Observable.Return(this.context
+    public override IObservable<IEnumerable<IPeripheral>> GetConnectedPeripherals(string? serviceUuid = null)
+        => Observable.Return(this.context
             .Manager
-            .Adapter!
-            .BondedDevices
-            .Where(x => x.Type == BluetoothDeviceType.Dual || x.Type == BluetoothDeviceType.Le)
+            .GetConnectedDevices(ProfileType.Gatt)
+            .Where(x => x.Type == BluetoothDeviceType.Dual || x.Type == BluetoothDeviceType.Le) // just in case
             .Select(this.context.GetDevice)
         );
 
 
-        public IObservable<bool> SetAdapterState(bool enable)
-        {
-            var result = false;
-            var ad = this.context.Manager.Adapter!;
-            if (enable && !ad.IsEnabled)
-                result =ad.Enable();
+    static readonly AndroidPermission[] permissions = new []
+    {
+        new AndroidPermission(Manifest.Permission.Bluetooth, null, null),
+        new AndroidPermission(Manifest.Permission.BluetoothAdmin, null, null),
+        new AndroidPermission(Manifest.Permission.BluetoothScan, 31, null),
+        new AndroidPermission(Manifest.Permission.BluetoothConnect, 31, null),
+        new AndroidPermission(Manifest.Permission.AccessFineLocation, null, 31)
+    };
+    public override IObservable<AccessState> RequestAccess() => Observable.FromAsync(async ct =>
+    {
+        if (!this.context.Android.EnsureAllManifestEntries(permissions))
+            return AccessState.NotSetup;
 
-            else if (!enable && ad.IsEnabled)
-                result = ad.Disable();
+        var results = await this.context
+            .Android
+            .RequestFilteredPermissions(permissions)
+            .ToTask(ct)
+            .ConfigureAwait(false);
 
-            return Observable.Return(result);
-        }
+        return results.IsSuccess()
+            ? this.context.Status // now look at the actual device state
+            : AccessState.Denied;
+    });
+
+
+    public override IObservable<ScanResult> Scan(ScanConfig? config = null)
+    {
+        if (this.IsScanning)
+            throw new ArgumentException("There is already an active scan");
+
+        return this
+            .RequestAccess()
+            .Do(access =>
+            {
+                if (access != AccessState.Available)
+                    throw new InvalidOperationException($"Invalid Status: {access} - Ensure you have the following permissions in your AndroidManifest.xml - android.permission.ACCESS_FINE_LOCATION, android.permission.BLUETOOTH_SCAN, & android.permission.BLUETOOTH_CONNECT");
+
+                this.IsScanning = true;
+            })
+            .SelectMany(_ => this.context.Scan(config ?? new ScanConfig()))
+            .Finally(() => this.IsScanning = false);
+    }
+
+
+    public override void StopScan()
+    {
+        this.context.StopScan();
+        this.isScanning = false;
+    }
+
+
+    public IObservable<IEnumerable<IPeripheral>> GetPairedPeripherals() => Observable.Return(this.context
+        .Manager
+        .Adapter!
+        .BondedDevices
+        .Where(x => x.Type == BluetoothDeviceType.Dual || x.Type == BluetoothDeviceType.Le)
+        .Select(this.context.GetDevice)
+    );
+
+
+    public IObservable<bool> SetAdapterState(bool enable)
+    {
+        var result = false;
+        var ad = this.context.Manager.Adapter!;
+        if (enable && !ad.IsEnabled)
+            result =ad.Enable();
+
+        else if (!enable && ad.IsEnabled)
+            result = ad.Disable();
+
+        return Observable.Return(result);
     }
 }
