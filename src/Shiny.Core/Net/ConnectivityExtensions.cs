3579f655d1496077599a68816eb00c8cd0bb2442
Allan Ritchie <allan.ritchie@gmail.com>
2022-07-28 12:28:53 -0400

    Update ConnectivityExtensions.cs

--- src/Shiny.Core/Net/ConnectivityExtensions.cs
+++ src/Shiny.Core/Net/ConnectivityExtensions.cs
@@ -7,8 +7,8 @@ namespace Shiny;
 
 public static class ConnectivityExtensions
 {
-    public static bool IsInternetAvailable(this IConnectivity connectivity)
-        => connectivity.Access.HasFlag(NetworkAccess.Internet) || connectivity.Access.HasFlag(NetworkAccess.ConstrainedInternet);
+    public static bool IsInternetAvailable(this IConnectivity connectivity, bool allowConstrained = true)
+        => connectivity.Access.HasFlag(NetworkAccess.Internet) || (allowConstrained && connectivity.Access.HasFlag(NetworkAccess.ConstrainedInternet));
 
 
     public static IObservable<bool> WhenInternetStatusChanged(this IConnectivity connectivity) => connectivity
