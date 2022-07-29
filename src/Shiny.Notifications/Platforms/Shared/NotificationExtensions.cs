7204321b092351b804876737d1f732e56fc62506
Allan Ritchie <allan.ritchie@gmail.com>
2022-07-29 14:58:29 -0400

    Improve notification registration & add ios configuration

--- src/Shiny.Notifications/Platforms/Shared/NotificationExtensions.cs
+++ src/Shiny.Notifications/Platforms/Shared/NotificationExtensions.cs
@@ -3,7 +3,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Shiny.Notifications;
 using Shiny.Notifications.Infrastructure;
-using Shiny.Stores;
 
 namespace Shiny;
 
@@ -13,48 +12,69 @@ public static class NotificationExtensions
     public static INotificationManager Notifications(this ShinyContainer container) => container.GetService<INotificationManager>();
 
 
+#if IOS || MACCATALYST
+    /// <summary>
+    /// Registers notification manager with Shiny
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="configuration"></param>
+    /// <returns></returns>
+    public static void AddNotifications<TDelegate>(this IServiceCollection services, IosConfiguration? configuration) where TDelegate : INotificationDelegate
+        => services.AddNotifications(typeof(TDelegate), configuration);
+
+
     /// <summary>
     /// Registers notification manager with Shiny
     /// </summary>
     /// <param name="services"></param>
     /// <param name="delegateType"></param>
+    /// <param name="configuration"></param>
     /// <returns></returns>
-    public static bool AddNotifications(this IServiceCollection services, Type? delegateType = null)
+    public static void AddNotifications(this IServiceCollection services, Type? delegateType = null, IosConfiguration? configuration = null)
     {
-#if IOS || MACCATALYST || ANDROID
+        services.AddSingleton(configuration ?? new());
         services.AddRepository<NotificationStoreConverter, Notification>();
         services.AddChannelManager();
         services.AddShinyService<NotificationManager>();
 
         if (delegateType != null)
             services.AddShinyService(delegateType);
+    }
 
-#if ANDROID
+#elif ANDROID
+
+    /// <summary>
+    /// Registers notification manager with Shiny
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="options"></param>
+    /// <returns></returns>
+    public static void AddNotifications<TDelegate>(this IServiceCollection services, AndroidCustomizationOptions? options = null) where TDelegate : INotificationDelegate
+        => services.AddNotifications(typeof(TDelegate), options);
+
+
+    /// <summary>
+    /// Registers notification manager with Shiny
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="delegateType"></param>
+    /// <param name="options"></param>
+    /// <returns></returns>
+    public static void AddNotifications(this IServiceCollection services, Type? delegateType = null, AndroidCustomizationOptions? options = null)
+    {
+        services.AddSingleton(options ?? new());
         services.AddGeofencing<NotificationGeofenceDelegate>();
         services.AddSingleton<INotificationCustomizer, DefaultAndroidNotificationCustomizer>();
         services.TryAddSingleton<AndroidNotificationProcessor>();
         services.TryAddSingleton<AndroidNotificationManager>();
-#endif
-        return true;
-#else
-        return false;
-#endif
+
+        services.AddRepository<NotificationStoreConverter, Notification>();
+        services.AddChannelManager();
+        services.AddShinyService<NotificationManager>();
+
+        if (delegateType != null)
+            services.AddShinyService(delegateType);
     }
 
-
-#if ANDROID
-    public static bool AddNotifications(this IServiceCollection services, AndroidCustomizationOptions options, Type? delegateType)
-    {
-        services.AddSingleton(options);
-        return services.AddNotifications(delegateType);
-    }
-
-
-    public static bool AddNotifications<T>(this IServiceCollection services, AndroidCustomizationOptions options) where T : INotificationDelegate
-        => services.AddNotifications(options, typeof(T));
-
 #endif
-
-    public static bool AddNotifications<T>(this IServiceCollection services) where T : INotificationDelegate
-        => services.AddNotifications(typeof(T));
 }
