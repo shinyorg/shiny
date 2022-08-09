9da94e6f03bddc2009b4c0e5b90b2296c821ab0c
Allan Ritchie <allan.ritchie@gmail.com>
2022-08-09 14:40:56 -0400

    WIP Blazor

--- tests/BlazorSample/Program.cs
+++ tests/BlazorSample/Program.cs
@@ -9,7 +9,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.UseShiny();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddBluetoothLE();
+//builder.Services.AddBluetoothLE();
 
 await builder.Build().RunAsync();
