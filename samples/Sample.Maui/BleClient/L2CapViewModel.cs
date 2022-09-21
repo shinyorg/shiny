eae68430a41a68cfb3951122a5bf0a1ddeb29d7a
Allan Ritchie <allan.ritchie@gmail.com>
2022-09-21 12:44:09 -0400

    L2Cap sample updates

--- samples/Sample.Maui/BleClient/L2CapViewModel.cs
+++ samples/Sample.Maui/BleClient/L2CapViewModel.cs
@@ -7,55 +7,94 @@ namespace Sample.BleClient;
 public class L2CapViewModel : ViewModel
 {
     IPeripheral peripheral = null!;
+    Task readerTask = null!;
     L2CapChannel? channel;
-
+    
 
     public L2CapViewModel(BaseServices services) : base(services)
     {
-        this.Toggle = ReactiveCommand.CreateFromTask(async () =>
-        {
-            if (this.IsConnected)
+        this.Write = ReactiveCommand.CreateFromTask(
+            async () =>
             {
-                await this.peripheral.WithConnectIf();
+                var bytes = Encoding.UTF8.GetBytes(this.WriteValue!);
+                await this.channel!.OutputStream.WriteAsync(bytes);
 
-                this.channel = await this.peripheral.TryOpenL2CapChannel(
-                    (ushort)this.Psm,
-                    this.Secure
-                );
-                if (this.channel == null)
-                    throw new InvalidOperationException("Could not open L2Cap channel");
+                this.Output = $"{this.WriteValue}{Environment.NewLine}{this.Output}";
+                this.WriteValue = String.Empty;
+            },
+            this.WhenAny(
+                x => x.IsConnected,
+                x => x.WriteValue,
+                (conn, wv) => conn.GetValue() && !wv.GetValue().IsEmpty()
+            )
+        );
 
-                Task.Run(async () =>
+        this.Toggle = ReactiveCommand.CreateFromTask(
+            async () =>
+            {
+                if (this.IsConnected)
                 {
-                    var buffer = new Memory<byte>();
-                    await this.channel.InputStream.ReadAsync(buffer, CancellationToken.None);
+                    await this.peripheral.WithConnectIf();
 
-                    this.Input = Encoding.UTF8.GetString(buffer.ToArray()) + Environment.NewLine + this.Input;
-                });
+                    this.channel = await this.peripheral
+                        .TryOpenL2CapChannel(
+                            (ushort)this.Psm,
+                            this.Secure
+                        )
+                        .ToTask();
 
-                this.IsConnected = true;
-                // TODO: start reader thread
-            }
-            else
-            {
-  
-            }
-        });
+                    if (this.channel == null)
+                        throw new InvalidOperationException("Could not open L2Cap channel");
+
+                    this.readerTask = Task.Run(async () =>
+                    {
+                        var buffer = new Memory<byte>();
+                        while (this.peripheral.IsConnected() && this.IsConnected)
+                        {
+                            await this.channel.InputStream.ReadAsync(buffer, CancellationToken.None).ConfigureAwait(false);
+                            this.AppendToInput(buffer.ToArray());                        
+                        }
+                    });
+
+                    this.IsConnected = true;
+                }
+                else
+                {
+                    this.IsConnected = false;
+                    this.channel?.Dispose();
+                    this.channel = null;
+                    this.readerTask = null!;
+                    this.peripheral.CancelConnection();
+                }
+            },
+            this.WhenAny(
+                x => x.Psm,
+                x => x.GetValue() > 0 && x.GetValue() <= ushort.MaxValue
+            )
+        );
     }
 
 
     [Reactive] public int Psm { get; set; }
     [Reactive] public bool Secure { get; set; }
     [Reactive] public bool IsConnected { get; private set; }
-    [Reactive] public string Output { get; set; }
+    [Reactive] public string Output { get; private set; }
     [Reactive] public string Input { get; private set; }
+    [Reactive] public string WriteValue { get; private set; }
+    public ICommand Write { get; }
     public ICommand Toggle { get; }
 
 
     public override Task InitializeAsync(INavigationParameters parameters)
     {
         this.peripheral = parameters.GetValue<IPeripheral>("Peripheral");
-
         return base.InitializeAsync(parameters);
     }
+
+
+    void AppendToInput(byte[] data) => this.Platform.InvokeOnMainThread(() =>
+    {
+        var value = Encoding.UTF8.GetString(data.ToArray());
+        this.Input = $"{value}{Environment.NewLine}{this.Input}";
+    });
 }
