edbefec2e82733758c87d2910d164c6f75769446
Allan Ritchie <allan.ritchie@gmail.com>
2022-09-22 01:11:19 -0400

    Sqlite logging & dialog on errors

--- samples/Sample.Maui/CommandExceptionHandler.cs
+++ samples/Sample.Maui/CommandExceptionHandler.cs
@@ -1,23 +1,45 @@
-﻿namespace Sample;
+﻿using Prism.Common;
+using Prism.Navigation.Xaml;
+
+namespace Sample;
+
 
 public class CommandExceptionHandler : IObserver<Exception>, IShinyStartupTask
 {
+    readonly IApplication application;
+    readonly IPlatform platform;
     readonly ILogger logger;
 
 
-    public CommandExceptionHandler(ILogger<CommandExceptionHandler> logger)
+    public CommandExceptionHandler(
+        IApplication application,
+        IPlatform platform,
+        ILogger<CommandExceptionHandler> logger
+    )
     {
+        this.application = application;
+        this.platform = platform;
         this.logger = logger;
     }
+
 
     public void Start() => RxApp.DefaultExceptionHandler = this;
     public void OnCompleted() { }
     public void OnError(Exception error) { }
-    public void OnNext(Exception value)
+    public async void OnNext(Exception value)
     {
         this.logger.LogError(value, "Command exception caught");
-        // TODO: dialogs
+        await this.Run(x => x.DisplayAlertAsync("ERROR", value.ToString(), "OK"));
     }
-    
-}
 
+
+    async Task Run(Func<IPageDialogService, Task> func)
+    {
+        var window = this.application.Windows.OfType<Window>().First();
+        var currentPage = MvvmHelpers.GetCurrentPage(window.Page);
+        var container = currentPage.GetContainerProvider();
+
+        var dialogs = container.Resolve<IPageDialogService>();
+        await this.platform.InvokeTaskOnMainThread(() => func(dialogs));
+    }
+}
---
+++ samples/Sample.Maui/Infrastructure/SqliteLoggerProvider.cs
@@ -0,0 +1,13 @@
+﻿using Shiny.Hosting;
+
+namespace Sample.Infrastructure;
+
+
+public class SqliteLoggerProvider : ILoggerProvider
+{
+    readonly LogLevel logLevel;
+    public SqliteLoggerProvider(LogLevel logLevel) => this.logLevel = logLevel;
+
+    public ILogger CreateLogger(string categoryName) => new SqliteLogger(this.logLevel, Host.Current.Services.GetRequiredService<SampleSqliteConnection>());
+    public void Dispose() { }
+}
