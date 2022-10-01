50113fef6b3a0a1cbc68bd1727c64ad26c6e5f7a
Allan Ritchie <allan.ritchie@gmail.com>
2022-10-01 12:55:34 -0400

    Fixes for beacon build post-BLE changes

--- src/Shiny.Beacons/Platforms/Android/BleManagerExtensions.cs
+++ src/Shiny.Beacons/Platforms/Android/BleManagerExtensions.cs
@@ -2,6 +2,8 @@
 using System.Linq;
 using System.Reactive.Linq;
 using Shiny.BluetoothLE;
+using Android.Bluetooth.LE;
+using ScanResult = Shiny.BluetoothLE.ScanResult;
 
 namespace Shiny.Beacons;
 
@@ -11,16 +13,12 @@ public static class BleManagerExtensions
     // TODO: pass multiple serviceUUIDs if scanning for different beacon types
     public static IObservable<Beacon> ScanForBeacons(this IBleManager manager, bool forMonitoring)
     {
-        var scanType = forMonitoring
-            ? BleScanType.LowLatency
-            : BleScanType.LowPowered;
-
-        var cfg = new ScanConfig { ScanType = scanType };
+        var scanMode = forMonitoring ? ScanMode.LowPower : ScanMode.Balanced;
         //if (config?.ScanServiceUuids?.Any() ?? false)
         //    cfg.ServiceUuids = config.ScanServiceUuids;
 
         return manager
-            .Scan(cfg)
+            .Scan(new AndroidScanConfig(scanMode))
             .Where(x => x.IsBeacon())
             .Select(x => x.AdvertisementData.ManufacturerData!.Data.Parse(x.Rssi));
     }
