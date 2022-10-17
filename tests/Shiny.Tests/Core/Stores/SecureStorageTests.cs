65b559c410c72836be6725f3d27f154a9321aaf4
Allan Ritchie <allan.ritchie@gmail.com>
2022-10-17 13:21:52 -0400

    Add secure store tests

---
+++ tests/Shiny.Tests/Core/Stores/SecureStorageTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using Shiny.Infrastructure.Impl;
+using Shiny.Stores;
+using Shiny.Stores.Impl;
+
+namespace Shiny.Tests.Core.Stores;
+
+
+public class SecureStorageTests
+{
+    readonly IKeyValueStore secureStore;
+
+    public SecureStorageTests()
+    {
+        var serializer = new DefaultSerializer();
+#if ANDROID
+        this.secureStore = new SecureKeyValueStore(new AndroidPlatform(), serializer);
+#else
+        this.secureStore = new SecureKeyValueStore(new IosPlatform(), serializer);
+#endif
+    }
+
+
+    [Fact]
+    public void RemoveWhereNotExists()
+    {
+        var key = Guid.NewGuid().ToString();
+        this.secureStore.Clear();
+        this.secureStore.Remove(key);
+        this.secureStore.Remove(key);
+    }
+
+
+    [Fact]
+    public void SetDuplicate()
+    {
+        var key = Guid.NewGuid().ToString();
+        this.secureStore.Clear();
+        this.secureStore.Set(key, "1");
+        this.secureStore.Set(key, "1");
+    }
+
+
+    [Fact]
+    public void WithBinding()
+    {
+        var factory = new KeyValueStoreFactory(new[] { this.secureStore });
+        var logger = Shiny.Hosting.Host.Current.Logging.CreateLogger<ObjectStoreBinder>();
+        var binder = new ObjectStoreBinder(factory, logger);
+        var obj = new TestBind();
+
+        try
+        {
+            binder.Bind(obj, this.secureStore);
+
+            obj.StringProperty = "test";
+            obj.StringProperty = null;
+        }
+        finally
+        {
+            binder.UnBind(obj);
+        }
+    }
+}
