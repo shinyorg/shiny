94e8990002c1f6ce7b46f1a93ad8ae0edaa61f04
Allan Ritchie <allan.ritchie@gmail.com>
2022-10-20 11:39:16 -0400

    Update ManagedScanViewModel.cs

--- samples/Sample.Maui/BleManaged/ManagedScanViewModel.cs
+++ samples/Sample.Maui/BleManaged/ManagedScanViewModel.cs
@@ -14,26 +14,46 @@ public class ManagedScanViewModel : ViewModel
     {
         // we are specifically scanning for VeePeak BLE OBD device
         this.scanner = bleManager
-            .CreateManagedScanner(
-                RxApp.MainThreadScheduler,
-                TimeSpan.FromSeconds(10),
-#if ANDROID
-                new AndroidScanConfig(
-                    Android.Bluetooth.LE.ScanMode.Opportunistic,
-                    true,
-                    ServiceUuid
-                )
-#else
-                new ScanConfig(
-                    ServiceUuids: ServiceUuid
-                )
-#endif
-            )
+            .CreateManagedScanner()
             .DisposedBy(this.DestroyWith);
 
         this.Toggle = ReactiveCommand.Create(async () =>
-            this.IsBusy = await this.scanner.Toggle()
-        );
+        {
+            if (this.IsBusy)
+            {
+                this.scanner.Stop();
+                Console.WriteLine("Stop Scan");
+            }
+            else
+            {
+                Console.WriteLine("Start Scan");
+                await this.scanner.Start(
+#if ANDROID
+                    new AndroidScanConfig(
+                        Android.Bluetooth.LE.ScanMode.Opportunistic,
+                        true,
+                        ServiceUuid
+                    ),
+#else
+                    new ScanConfig(
+                        ServiceUuids: ServiceUuid
+                    ),
+#endif
+                    x =>
+                    {
+                        // optional predicate filter
+                        var found = true;
+                        var pName = (x.Peripheral.Name ?? "None");
+                        Console.WriteLine("Peripheral Name: " + pName);
+                        return found;
+                    },
+                    RxApp.MainThreadScheduler,
+                    null, // buffer time - default is 3 seconds
+                    TimeSpan.FromSeconds(10)
+                );
+            }
+            this.IsBusy = this.scanner.IsScanning;
+        });
 
         this.WhenAnyValueSelected(
             x => x.SelectedPeripheral,
@@ -50,7 +70,7 @@ public class ManagedScanViewModel : ViewModel
 
 
     public ICommand Toggle { get; }
-    public ObservableCollection<ManagedScanResult> Peripherals => this.scanner.Peripherals;
+    public INotifyReadOnlyCollection<ManagedScanResult> Peripherals => this.scanner.Peripherals;
     [Reactive] public ManagedScanResult? SelectedPeripheral { get; set; }
 }
 
