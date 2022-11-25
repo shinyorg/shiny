a9c3b23c7f7fd58236103147959cf6f977811db3
Allan Ritchie <allan.ritchie@gmail.com>
2022-11-25 12:00:22 -0500

    Move hosting to proper locations

---
+++ src/Shiny.Hosting.Avalonia/ShinyAvaloniaApp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Shiny.Hosting;
+
+namespace Shiny;
+
+
+public abstract class ShinyAvaloniaApp : Avalonia.Application
+{
+    protected abstract IHost CreateShinyHost();
+
+
+    public override void Initialize()
+    {
+        // TODO: how to wire up lifecycles?
+        this.CreateShinyHost();
+        base.Initialize();
+    }
+}
+
