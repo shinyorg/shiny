ee06383c6a13699dd061fef471a510e9dada64b8
Allan Ritchie <allan.ritchie@gmail.com>
2022-12-01 13:47:53 -0500

    Load platform specific appsettings

--- src/Shiny.Extensions.Configuration/Platforms/Android/ConfigurationBuilderExtensions.cs
+++ src/Shiny.Extensions.Configuration/Platforms/Android/ConfigurationBuilderExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Android.App;
+using Java.Nio.FileNio.Attributes;
 using Shiny.Extensions.Configuration;
 using System;
 using System.Linq;
@@ -15,7 +16,25 @@ public static partial class ConfigurationBuilderExtensions
     /// <param name="fileName"></param>
     /// <param name="optional"></param>
     /// <returns></returns>
-    public static IConfigurationBuilder AddJsonAndroidAsset(this IConfigurationBuilder builder, string fileName = "appsettings.json", bool optional = true)
+    public static IConfigurationBuilder AddJsonAndroidAsset(this IConfigurationBuilder builder, bool optional = true, bool reloadOnChange = true, bool includePlatformSpecific = true)
+    {
+        if (includePlatformSpecific)
+            builder.AddJsonAssetInternal("appsettings.android.json", true, reloadOnChange);
+
+        return builder.AddJsonAssetInternal("appsettings.json", optional, reloadOnChange);
+    }
+
+
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="builder"></param>
+    /// <returns></returns>
+    public static IConfigurationBuilder AddAndroidPreferences(this IConfigurationBuilder builder)
+        => builder.Add(new SharedPreferencesConfigurationSource());
+
+
+    static IConfigurationBuilder AddJsonAssetInternal(this IConfigurationBuilder builder, string fileName, bool optional, bool reloadOnChange)
     {
         var assets = Application.Context.Assets;
         var exists = assets!
@@ -30,13 +49,4 @@ public static partial class ConfigurationBuilderExtensions
 
         return builder;
     }
-
-
-    /// <summary>
-    ///
-    /// </summary>
-    /// <param name="builder"></param>
-    /// <returns></returns>
-    public static IConfigurationBuilder AddAndroidPreferences(this IConfigurationBuilder builder)
-        => builder.Add(new SharedPreferencesConfigurationSource());
 }
---
+++ src/Shiny.Extensions.Configuration/Platforms/Apple/ConfigurationBuilderExtensions.cs
@@ -0,0 +1,50 @@
+﻿using Foundation;
+using Shiny.Extensions.Configuration;
+using System.IO;
+
+namespace Microsoft.Extensions.Configuration;
+
+
+public static partial class ConfigurationBuilderExtensions
+{
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="builder"></param>
+    /// <param name="fileName"></param>
+    /// <param name="optional"></param>
+    /// <returns></returns>
+    public static IConfigurationBuilder AddJsonIosBundle(this IConfigurationBuilder builder, bool optional = true, bool reloadOnChange = true, bool includePlatformSpecific = true)
+    {
+        if (includePlatformSpecific)
+        {
+            builder.AddJsonFileInternal("appsettings.apple.json", false, reloadOnChange);
+#if IOS
+            builder.AddJsonFileInternal("appsettings.ios.json", false, reloadOnChange);
+#elif MACCATALYST
+            builder.AddJsonFileInternal("appsettings.maccatalyst.json", optional, reloadOnChange);
+#endif
+        }
+        return builder.AddJsonFileInternal("appsettings.json", optional, reloadOnChange);
+    }
+
+
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="builder"></param>
+    /// <returns></returns>
+    public static IConfigurationBuilder AddIosUserDefaults(this IConfigurationBuilder builder)
+        => builder.Add(new NSUserDefaultsConfigurationSource());
+
+
+    static IConfigurationBuilder AddJsonFileInternal(this IConfigurationBuilder builder, string fileName, bool optional, bool reloadOnChange)
+    {
+#if MACCATALYST
+        var path = Path.Combine(NSBundle.MainBundle.BundlePath, "Contents", "Resources", fileName);
+#else
+        var path = Path.Combine(NSBundle.MainBundle.BundlePath, fileName);
+#endif
+        return builder.AddJsonFile(path, optional, reloadOnChange);
+    } 
+}
