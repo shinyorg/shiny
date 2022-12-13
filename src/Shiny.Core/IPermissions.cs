cc90e06b45c8d8dcb4162468a0e63011a9069a21
Allan Ritchie <allan.ritchie@gmail.com>
2022-12-13 13:04:33 -0500

    Remove shiny container

--- src/Shiny.Core/IPermissions.cs
+++ src/Shiny.Core/IPermissions.cs
@@ -1,33 +1,33 @@
-﻿using System.Threading.Tasks;
+﻿//using System.Threading.Tasks;
 
-namespace Shiny;
-
-
-public interface IPermissions
-{
-    /// <summary>
-    /// Will set or replace any current permission checks
-    /// </summary>
-    /// <typeparam name="TPermission"></typeparam>
-    /// <param name="permissionName"></param>
-    void Request(IPermission permission);
-
-}
+//namespace Shiny;
 
 
-// TODO: android needs access to top activity
-// TODO: ios needs mainthread
-public interface IPermission
-{
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <returns></returns>
-    Task Request(string permissionName);
+//public interface IPermissions
+//{
+//    /// <summary>
+//    /// Will set or replace any current permission checks
+//    /// </summary>
+//    /// <typeparam name="TPermission"></typeparam>
+//    /// <param name="permissionName"></param>
+//    void Request(IPermission permission);
 
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <returns></returns>
-    Task GetCurrent();
-}
+//}
+
+
+//// TODO: android needs access to top activity
+//// TODO: ios needs mainthread
+//public interface IPermission
+//{
+//    /// <summary>
+//    /// 
+//    /// </summary>
+//    /// <returns></returns>
+//    Task Request(string permissionName);
+
+//    /// <summary>
+//    /// 
+//    /// </summary>
+//    /// <returns></returns>
+//    Task GetCurrent();
+//}
---
+++ src/Shiny.Hosting.Xamarin/ShinyContainer.cs
@@ -0,0 +1,48 @@
+﻿//using System;
+
+//using Microsoft.Extensions.DependencyInjection;
+//using Microsoft.Extensions.Logging;
+//using Shiny.Hosting;
+
+//namespace Shiny;
+
+
+//public class ShinyContainer
+//{
+
+//    static ShinyContainer? current;
+//    public static ShinyContainer Current
+//    {
+//        get
+//        {
+//            if (current == null)
+//                throw new InvalidOperationException("ShinyContainer does not appear to be initialized.  Make sure to call Init first");
+
+//            return current;
+//        }
+//    }
+
+
+//    public T GetService<T>() => this.Services.GetRequiredService<T>()!;
+//    public ILoggerFactory Logging => this.Host.Logging;
+//    public IServiceProvider Services => this.Host.Services;
+//    public IHost Host { get; init; }
+
+
+//    /// <summary>
+//    /// Initialize a new static container - not recommended for use with MAUI or any type of DI centric framework like Prism
+//    /// </summary>
+//    /// <param name="buildAction"></param>
+//    public static void Init(Action<IHostBuilder> buildAction)
+//    {
+//        var builder = new HostBuilder();
+//        buildAction(builder);
+//        var host = builder.Build();
+//        host.Run();
+
+//        current = new ShinyContainer
+//        {
+//            Host = host
+//        };
+//    }
+//}
