fd9acae39c0b3a672e1ed9435560d5d0ccfddd2e
Allan Ritchie <allan.ritchie@gmail.com>
2023-04-06 20:25:10 -0400

    More classic xam fixes

--- samples/Sample.Maui/Platforms/Android/MainActivity.cs
+++ samples/Sample.Maui/Platforms/Android/MainActivity.cs
@@ -4,7 +4,17 @@ using Android.OS;
 
 namespace Sample.Maui;
 
-[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+[Activity(
+    Theme = "@style/Maui.SplashTheme",
+    MainLauncher = true,
+    ConfigurationChanges =
+        ConfigChanges.ScreenSize |
+        ConfigChanges.Orientation |
+        ConfigChanges.UiMode |
+        ConfigChanges.ScreenLayout |
+        ConfigChanges.SmallestScreenSize |
+        ConfigChanges.Density
+)]
 public class MainActivity : MauiAppCompatActivity
 {
 }
