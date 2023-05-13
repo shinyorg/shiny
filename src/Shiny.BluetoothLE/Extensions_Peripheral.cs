ff67b405b5ed7100380ace651680788167617287
Allan Ritchie <allan.ritchie@gmail.com>
2023-05-12 20:29:38 -0400

    BLE Client Some docs and missing arg for extension passthrough

--- src/Shiny.BluetoothLE/Extensions_Peripheral.cs
+++ src/Shiny.BluetoothLE/Extensions_Peripheral.cs
@@ -42,7 +42,7 @@ public static class PeripheralExtensions
     /// </summary>
     /// <param name="peripheral"></param>
     /// <param name="config"></param>
-    /// <returns></returns>
+    /// <returns>A completing observable</returns>
     public static IObservable<IPeripheral> WithConnectIf(this IPeripheral peripheral, ConnectionConfig? config = null) => Observable.Create<IPeripheral>(ob =>
     {
         if (peripheral.Status == ConnectionState.Connected)
@@ -77,7 +77,7 @@ public static class PeripheralExtensions
     /// Quick helper around WhenStatusChanged().Where(x => x == ConnectionStatus.Connected)
     /// </summary>
     /// <param name="peripheral"></param>
-    /// <returns></returns>
+    /// <returns>A streaming observable - this will pump connected if subscribing when connected</returns>
     public static IObservable<IPeripheral> WhenConnected(this IPeripheral peripheral) =>
         peripheral
             .WhenStatusChanged()
@@ -89,7 +89,7 @@ public static class PeripheralExtensions
     /// Quick helper around WhenStatusChanged().Where(x => x == ConnectionStatus.Disconnected)
     /// </summary>
     /// <param name="peripheral"></param>
-    /// <returns></returns>
+    /// <returns>A streaming observable - will bump disconnected if hooking when already disconnected</returns>
     public static IObservable<IPeripheral> WhenDisconnected(this IPeripheral peripheral) =>
         peripheral
             .WhenStatusChanged()
@@ -97,21 +97,66 @@ public static class PeripheralExtensions
             .Select(_ => peripheral);
 
 
-    public static IObservable<BleCharacteristicResult> NotifyCharacteristic(this IPeripheral peripheral, BleCharacteristicInfo info, bool useIndicationsIfAvailable = true)
-        => peripheral.NotifyCharacteristic(info.Service.Uuid, info.Uuid, useIndicationsIfAvailable);
+    /// <summary>
+    /// Hooks a notification subscription - NOTE: this is a refcount observable. A characteristic will stay hooked
+    /// as long as their are subscribers
+    /// </summary>
+    /// <param name="peripheral"></param>
+    /// <param name="info"></param>
+    /// <param name="useIndicationsIfAvailable"></param>
+    /// <param name="emitSubscribedEvent">
+    /// If true, the notificaton BleCharacteristicEvent will be emitted with empty data and notification subscribed - useful for knowing when hook is made
+    /// WARNING: If subscriber has multiple hooks, this event will only be emitted to the first subscriber
+    /// </param>
+    /// <returns>A streaming observable</returns>
+    public static IObservable<BleCharacteristicResult> NotifyCharacteristic(this IPeripheral peripheral, BleCharacteristicInfo info, bool useIndicationsIfAvailable = true, bool emitSubscribedEvent = false)
+        => peripheral.NotifyCharacteristic(info.Service.Uuid, info.Uuid, useIndicationsIfAvailable, emitSubscribedEvent);
 
+    /// <summary>
+    /// Read a characteristic value
+    /// </summary>
+    /// <param name="peripheral"></param>
+    /// <param name="info"></param>
+    /// <returns>A completing observable</returns>
     public static IObservable<BleCharacteristicResult> ReadCharacteristic(this IPeripheral peripheral, BleCharacteristicInfo info)
         => peripheral.ReadCharacteristic(info.Service.Uuid, info.Uuid);
 
-    public static IObservable<BleCharacteristicResult> WriteCharacteristic(this IPeripheral peripheral, BleCharacteristicInfo info, byte[] data, bool withoutResponse = false)
-        => peripheral.WriteCharacteristic(info.Service.Uuid, info.Uuid, data, withoutResponse);
+    /// <summary>
+    /// Writes a value to a characteristic
+    /// </summary>
+    /// <param name="peripheral"></param>
+    /// <param name="info"></param>
+    /// <param name="data"></param>
+    /// <param name="withResponse"></param>
+    /// <returns>A completing observable</returns>
+    public static IObservable<BleCharacteristicResult> WriteCharacteristic(this IPeripheral peripheral, BleCharacteristicInfo info, byte[] data, bool withResponse = true)
+        => peripheral.WriteCharacteristic(info.Service.Uuid, info.Uuid, data, withResponse);
 
+    /// <summary>
+    /// Get all descriptors
+    /// </summary>
+    /// <param name="peripheral"></param>
+    /// <param name="info"></param>
+    /// <returns>A completing observable</returns>
     public static IObservable<IReadOnlyList<BleDescriptorInfo>> GetDescriptors(this IPeripheral peripheral, BleCharacteristicInfo info)
         => peripheral.GetDescriptors(info.Service.Uuid, info.Uuid);
 
+    /// <summary>
+    /// Write a value to a descriptor
+    /// </summary>
+    /// <param name="peripheral"></param>
+    /// <param name="info"></param>
+    /// <param name="data"></param>
+    /// <returns>A completing observable</returns>
     public static IObservable<BleDescriptorResult> WriteDescriptor(this IPeripheral peripheral, BleDescriptorInfo info, byte[] data)
         => peripheral.WriteDescriptor(info.Characteristic.Service.Uuid, info.Characteristic.Uuid, info.Uuid, data);
 
+    /// <summary>
+    /// Read a descriptor value
+    /// </summary>
+    /// <param name="peripheral"></param>
+    /// <param name="info"></param>
+    /// <returns>A completing observable with the byte array from the read operation</returns>
     public static IObservable<BleDescriptorResult> ReadDescriptor(this IPeripheral peripheral, BleDescriptorInfo info)
         => peripheral.ReadDescriptor(info.Characteristic.Service.Uuid, info.Characteristic.Uuid, info.Uuid);
 }
