5850c83e68208efd7920f8f6b8149f479fe75cfc
Allan Ritchie <allan.ritchie@gmail.com>
2023-06-22 16:57:32 -0400

    Deferred GPS delegate feature for later

---
+++ src/Shiny.Locations/DeferredGpsDelegate.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Shiny.Locations;
+
+
+public abstract class DeferredGpsDelegate : NotifyPropertyChanged, IGpsDelegate
+{
+    readonly DeferredGpsConfiguration configuration;
+    readonly ILogger logger;
+
+
+    public DeferredGpsDelegate(IServiceProvider services)
+    {
+        this.configuration = services.GetRequiredService<DeferredGpsConfiguration>();
+        this.logger = services.GetRequiredService<ILogger<DeferredGpsDelegate>>();
+    }
+
+
+    public async Task OnReading(GpsReading reading)
+    {
+        var fireReading = false;
+
+        if (this.LastReading == null)
+        {
+            fireReading = true;
+            this.logger.LogDebug("No previous reading");
+        }
+        else
+        {
+            if (this.configuration.MinDistance != null)
+            {
+                var dist = this.LastReading.Position.GetDistanceTo(reading.Position);
+                fireReading = dist >= this.configuration.MinDistance;
+
+                this.logger.DeferDistanceInfo(this.configuration.MinDistance!.TotalMeters, dist.TotalMeters, fireReading);
+            }
+
+            if (!fireReading && this.configuration.MinTime != null)
+            {
+                var timeDiff = reading.Timestamp.Subtract(this.LastReading.Timestamp);
+                fireReading = timeDiff >= this.configuration.MinTime;
+
+                this.logger.DeferTimeInfo(this.configuration.MinTime!.Value, timeDiff, fireReading);
+            }
+        }
+
+        if (fireReading)
+        {
+            try
+            {
+                await this.OnDeferredReading(reading).ConfigureAwait(false);
+            }
+            finally
+            {
+                this.LastReading = reading;
+            } 
+        }
+    }
+
+
+    GpsReading? lastReading;
+    public GpsReading? LastReading
+    {
+        get => this.lastReading;
+        set => this.Set(ref this.lastReading, value);
+    }
+
+
+    protected abstract Task OnDeferredReading(GpsReading reading);
+}
+
+// maybe make this mutable so it can be changed in app
+public record DeferredGpsConfiguration(
+    Distance? MinDistance,
+    TimeSpan? MinTime
+);
---
+++ src/Shiny.Locations/Log.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace Shiny.Locations;
+
+
+internal static partial class Log
+{
+    [LoggerMessage(
+        EventId = 1,
+        Level = LogLevel.Debug,
+        Message = "Deferred Distance: Min: {minMeters}. Current: {meters} - FIRE EVENT: {fireEvent}"
+    )]
+    public static partial void DeferDistanceInfo(this ILogger logger, double minMeters, double meters, bool fireEvent);
+
+
+    [LoggerMessage(
+        EventId = 2,
+        Level = LogLevel.Debug,
+        Message = "Deferred Time: Min: {minTime}. Current: {currentTime} - FIRE EVENT: {fireEvent}"
+    )]
+    public static partial void DeferTimeInfo(this ILogger logger, TimeSpan minTime, TimeSpan currentTime, bool fireEvent);
+}
