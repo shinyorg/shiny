5b8b3f2409d30d2dfedc608754024784fc662929
Allan Ritchie <allan.ritchie@gmail.com>
2023-07-14 14:13:56 -0400

    Fix classic ios

--- src/Shiny.BluetoothLE/Platforms/Apple/Extensions.cs
+++ src/Shiny.BluetoothLE/Platforms/Apple/Extensions.cs
@@ -5,25 +5,6 @@ namespace Shiny.BluetoothLE;
 
 internal static class PlatformExtensions
 {
-#if XAMARIN
-    public static bool IsUnknown(this CBCentralManagerState state)
-        => state == CBCentralManagerState.Unknown;
-
-
-    public static AccessState FromNative(this CBCentralManagerState state) => state switch
-    {
-        CBCentralManagerState.Resetting => AccessState.Available,
-        CBCentralManagerState.PoweredOn => AccessState.Available,
-        CBCentralManagerState.PoweredOff => AccessState.Disabled,
-        CBCentralManagerState.Unauthorized => AccessState.Denied,
-        CBCentralManagerState.Unsupported => AccessState.NotSupported,
-        _ => AccessState.Unknown
-    };
-#else
-    public static bool IsUnknown(this CBManagerState state)
-        => state == CBManagerState.Unknown;
-
-
     public static bool Is(this CBCharacteristic native, string serviceUuid, string characteristicUuid)
     {
         var nsUuid = CBUUID.FromString(serviceUuid);
@@ -43,6 +24,24 @@ internal static class PlatformExtensions
 
         return true;
     }
+
+#if XAMARIN
+    public static bool IsUnknown(this CBCentralManagerState state)
+        => state == CBCentralManagerState.Unknown;
+
+
+    public static AccessState FromNative(this CBCentralManagerState state) => state switch
+    {
+        CBCentralManagerState.Resetting => AccessState.Available,
+        CBCentralManagerState.PoweredOn => AccessState.Available,
+        CBCentralManagerState.PoweredOff => AccessState.Disabled,
+        CBCentralManagerState.Unauthorized => AccessState.Denied,
+        CBCentralManagerState.Unsupported => AccessState.NotSupported,
+        _ => AccessState.Unknown
+    };
+#else
+    public static bool IsUnknown(this CBManagerState state)
+        => state == CBManagerState.Unknown;
 
 
     public static AccessState FromNative(this CBManagerState state) => state switch
