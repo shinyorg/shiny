f773426b7298f147403e78343a687bf54c6de9eb
Allan Ritchie <allan.ritchie@gmail.com>
2023-07-25 11:22:27 -0400

    Repository count watcher extension

--- src/Shiny.Support.Repositories/Extensions.cs
+++ src/Shiny.Support.Repositories/Extensions.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Reactive.Linq;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Shiny.Support.Repositories;
@@ -19,4 +20,37 @@ public static class RepositoryExtensions
 
     public static bool Remove<T>(this IRepository repository, T item) where T : IRepositoryEntity
         => repository.Remove<T>(item.Identifier);
+
+
+    public static IObservable<int> CreateCountWatcher<T>(this IRepository repository) where T : IRepositoryEntity
+    {
+        var count = repository.GetList<T>().Count;
+
+        return repository
+            .WhenActionOccurs()
+            .Where(x =>
+                x.EntityType == typeof(T) &&
+                x.Action != RepositoryAction.Update
+            )
+            .Select(x =>
+            {
+                switch (x.Action)
+                {
+                    case RepositoryAction.Add:
+                        count++;
+                        break;
+
+                    case RepositoryAction.Remove:
+                        count--;
+                        break;
+
+                    case RepositoryAction.Clear:
+                        count = 0;
+                        break;
+                }
+
+                return count;
+            })
+            .StartWith(count);
+    }
 }
