2052f55aa55548f27b9237eb616f5c0549b84b79
Allan Ritchie <allan.ritchie@gmail.com>
2023-08-18 11:59:23 -0400

    Apple configuration improvements

--- src/Shiny.Net.Http/Platforms/Apple/AppleConfiguration.cs
+++ src/Shiny.Net.Http/Platforms/Apple/AppleConfiguration.cs
@@ -5,10 +5,13 @@ namespace Shiny.Net.Http;
 
 
 public record AppleConfiguration(
-    int? HttpMaximumConnectionsPerHost = null,
+    string? SessionName = null,
+    int HttpMaximumConnectionsPerHost = 2,
     NSUrlRequestCachePolicy? CachePolicy = null,
     bool AllowsCellularAccess = true,
     bool AllowsConstrainedNetworkAccess = true,
     bool AllowsExpensiveNetworkAccess = true,
-    bool? HttpShouldUsePipelining = null
+    bool? HttpShouldUsePipelining = null,
+    bool? ShouldUseExtendedBackgroundIdleMode = null,
+    bool? WaitsForConnectivity = null
 );
