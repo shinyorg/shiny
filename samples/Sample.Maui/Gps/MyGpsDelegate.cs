ea12de8ae5b097121ccffd90fdfc2d29ef66beea
Allan Ritchie <allan.ritchie@gmail.com>
2023-11-07 22:22:20 -0500

    Fix base job

--- samples/Sample.Maui/Gps/MyGpsDelegate.cs
+++ samples/Sample.Maui/Gps/MyGpsDelegate.cs
@@ -3,18 +3,35 @@
 namespace Sample.Gps;
 
 
-public partial class MyGpsDelegate : IGpsDelegate
+public partial class MyGpsDelegate : GpsDelegate
 {
     readonly SampleSqliteConnection conn;
-    public MyGpsDelegate(SampleSqliteConnection conn) => this.conn = conn;
+    public MyGpsDelegate(ILogger<MyGpsDelegate> logger, SampleSqliteConnection conn) : base(logger)
+    {
+        this.conn = conn;
+        this.MinimumDistance = Distance.FromMeters(10);
+        this.MinimumTime = TimeSpan.FromSeconds(10);
+    }
 
-    public Task OnReading(GpsReading reading) => this.conn.Log(
+    protected override Task OnGpsReading(GpsReading reading) => this.conn.Log(
         "GPS",
         $"{reading.Position.Latitude} / {reading.Position.Longitude} - H: {reading.Heading}",
         $"Accuracy: {reading.PositionAccuracy} - SP: {reading.Speed}",
         reading.Timestamp
     );
 }
+//public partial class MyGpsDelegate : IGpsDelegate
+//{
+//    readonly SampleSqliteConnection conn;
+//    public MyGpsDelegate(SampleSqliteConnection conn) => this.conn = conn;
+
+//    public Task OnReading(GpsReading reading) => this.conn.Log(
+//        "GPS",
+//        $"{reading.Position.Latitude} / {reading.Position.Longitude} - H: {reading.Heading}",
+//        $"Accuracy: {reading.PositionAccuracy} - SP: {reading.Speed}",
+//        reading.Timestamp
+//    );
+//}
 
 #if ANDROID
 public partial class MyGpsDelegate : IAndroidForegroundServiceDelegate
--- samples/Sample.Maui/Jobs/SampleJob.cs
+++ samples/Sample.Maui/Jobs/SampleJob.cs
@@ -1,32 +1,52 @@
-﻿using System;
-using System.Threading;
-using System.Threading.Tasks;
-using Shiny;
-using Shiny.Jobs;
+﻿using Shiny.Jobs;
 using Shiny.Notifications;
 
 namespace Sample;
 
 
-public class SampleJob : IJob
+public partial class SampleJob : Job
 {
     readonly INotificationManager notificationManager;
-    public SampleJob(INotificationManager notificationManager)
-        => this.notificationManager = notificationManager;
 
-    // TODO: test new object binding
+    public SampleJob(ILogger<SampleJob> logger, INotificationManager notificationManager) : base(logger)
+    { 
+        this.notificationManager = notificationManager;
+        this.MinimumTime = TimeSpan.FromSeconds(50);
+    }
 
-    public async Task Run(JobInfo jobInfo, CancellationToken cancelToken)
+    protected override async Task Run(CancellationToken cancelToken)
     {
         await this.notificationManager.Send(
             "Jobs",
-            $"Job Started - {jobInfo.Identifier}"
+            $"Job Started - {this.JobInfo!.Identifier}"
         );
-        //await Task.Delay(TimeSpan.FromSeconds(seconds), cancelToken);
 
         await this.notificationManager.Send(
             "Jobs",
-            $"Job Finished - {jobInfo.Identifier}"
+            $"Job Finished - {this.JobInfo!.Identifier}"
         );
     }
 }
+
+//public class SampleJob : IJob
+//{
+//    readonly INotificationManager notificationManager;
+//    public SampleJob(INotificationManager notificationManager)
+//        => this.notificationManager = notificationManager;
+
+//    // TODO: test new object binding
+
+//    public async Task Run(JobInfo jobInfo, CancellationToken cancelToken)
+//    {
+//        await this.notificationManager.Send(
+//            "Jobs",
+//            $"Job Started - {jobInfo.Identifier}"
+//        );
+//        //await Task.Delay(TimeSpan.FromSeconds(seconds), cancelToken);
+
+//        await this.notificationManager.Send(
+//            "Jobs",
+//            $"Job Finished - {jobInfo.Identifier}"
+//        );
+//    }
+//}
