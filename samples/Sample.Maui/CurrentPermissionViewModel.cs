0678f9a91f0dd14120c4879cb3eaa5e0965d5a60
Allan Ritchie <allan.ritchie@gmail.com>
2023-11-25 10:46:26 -0500

    Improve testing model

--- samples/Sample.Maui/CurrentPermissionViewModel.cs
+++ samples/Sample.Maui/CurrentPermissionViewModel.cs
@@ -17,28 +17,73 @@ public class CurrentPermissionViewModel : ViewModel
     {
         this.Refresh = ReactiveCommand.Create(() =>
         {
-            this.BleClient = bleManager.CurrentAccess;
-            this.BleHostAdvertised = bleHostingManager.AdvertisingAccessStatus;
-            this.BleHostGatt = bleHostingManager.GattAccessStatus;
-            this.GpsInApp = gpsManager.GetCurrentStatus(GpsRequest.Foreground);
-            this.GpsBackground = gpsManager.GetCurrentStatus(GpsRequest.Realtime(false));
-            this.Geofencing = geofenceManager.CurrentStatus;
+            this.PermissionSet = new List<PermissionViewModel> {
+                new PermissionViewModel(
+                    "BLE Client",
+                    () => bleManager.CurrentAccess,
+                    () => bleManager.RequestAccess().ToTask()
+                ),
+                new PermissionViewModel(
+                    "BLE Host - Advertising",
+                    () => bleHostingManager.AdvertisingAccessStatus,
+                    () => bleHostingManager.RequestAccess(true, false)
+                ),
+                new PermissionViewModel(
+                    "BLE Host - GATT",
+                    () => bleHostingManager.GattAccessStatus,
+                    () => bleHostingManager.RequestAccess(false, true)
+                ),
+                new PermissionViewModel(
+                    "GPS - In App",
+                    () => gpsManager.GetCurrentStatus(GpsRequest.Foreground),
+                    () => gpsManager.RequestAccess(GpsRequest.Foreground)
+                ),
+                new PermissionViewModel(
+                    "GPS - Background",
+                    () => gpsManager.GetCurrentStatus(GpsRequest.Realtime(false)),
+                    () => gpsManager.RequestAccess(GpsRequest.Realtime(false))
+                ),
+                new PermissionViewModel(
+                    "Geofencing",
+                    () => geofenceManager.CurrentStatus,
+                    () => geofenceManager.RequestAccess()
+                )
+            };
+            this.RaisePropertyChanged(nameof(this.PermissionSet));
         });
     }
 
 
     public ICommand Refresh { get; }
-    [Reactive] public AccessState BleClient { get; private set; }
-    [Reactive] public AccessState BleHostGatt { get; private set; }
-    [Reactive] public AccessState BleHostAdvertised { get; private set; }
-    [Reactive] public AccessState GpsInApp { get; private set; }
-    [Reactive] public AccessState GpsBackground { get; private set; }
-    [Reactive] public AccessState Geofencing { get; private set; }
-
+    public List<PermissionViewModel> PermissionSet { get; private set; } 
 
     public override void OnAppearing()
     {
         base.OnAppearing();
         this.Refresh.Execute(null);
     }
+}
+
+
+public class PermissionViewModel : ReactiveObject
+{
+    public PermissionViewModel(
+        string description,
+        Func<AccessState> getCurrent,
+        Func<Task<AccessState>> requestAccess
+    )
+    {
+        this.Description = description;
+        this.Status = getCurrent.Invoke();
+        this.Request = ReactiveCommand.CreateFromTask(async () =>
+        {
+            await requestAccess.Invoke();
+            this.Status = getCurrent();
+        });
+    }
+
+
+    public ICommand Request { get; }
+    public string Description { get; }
+    [Reactive] public AccessState Status { get; private set; }
 }
