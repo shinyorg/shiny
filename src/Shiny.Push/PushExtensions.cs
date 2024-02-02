de07ceaea1a6cc7a9509e98db2a7c151c0ed1c3a
Allan Ritchie <allan.ritchie@gmail.com>
2024-02-02 10:41:49 -0500

    Babysitting a user that can't be bothered to look for themselves

---
+++ src/Shiny.Push/PushExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+
+namespace Shiny.Push;
+
+
+public static class PushExtensions
+{
+    public static bool IsTagsSupport(this IPushManager push)
+        => push.Tags != null;
+
+    public static async Task<PushAccessState> TryRequestAccessWithTags(this IPushManager pushManager, params string[] tags)
+    {
+        var access = await pushManager
+            .RequestAccess()
+            .ConfigureAwait(false);
+
+        if (pushManager.Tags != null)
+        {
+            foreach (var tag in tags)
+            {
+                await pushManager
+                    .Tags
+                    .AddTag(tag)
+                    .ConfigureAwait(false);
+            }
+        }
+        return access;
+    }
+}
