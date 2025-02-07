dbc89a584fc3477278ad4d2dbd6e1be9adf99826
Allan Ritchie <allan.ritchie@gmail.com>
2025-02-07 11:01:52 -0500

    Add uno hosting

---
+++ src/Shiny.Hosting.Uno/UnoExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace Shiny.Hosting.Uno;
+
+public static class UnoExtensions
+{
+    public static IHostBuilder AddShiny(this IHostBuilder hostBuilder)
+    {
+        return hostBuilder;
+    }
+}
+
