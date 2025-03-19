8b61cff8a299bb5e12b60b92a37339638152e762
Allan Ritchie <allan.ritchie@gmail.com>
2025-03-18 23:51:44 -0400

    GPS delegate and semaphore synchronization

--- src/Shiny.Locations/GpsDelegate.cs
+++ src/Shiny.Locations/GpsDelegate.cs
@@ -1,62 +1,113 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 
 namespace Shiny.Locations;
 
 
-public abstract class GpsDelegate : NotifyPropertyChanged, IGpsDelegate
+public abstract class GpsDelegate(ILogger logger) : NotifyPropertyChanged, IGpsDelegate
 {
-    
-    public GpsDelegate(ILogger logger) => this.Logger = logger;
-
-
-    protected ILogger Logger { get; }
+    readonly SemaphoreSlim semaphore = new(1);
+    protected ILogger Logger => logger;
 
 
     public async Task OnReading(GpsReading reading)
     {
-        var fireReading = false;
-
-        if (this.LastReading == null)
+        try
         {
-            fireReading = true;
-            this.Logger.LogDebug("No previous reading");
+            await this.semaphore.WaitAsync().ConfigureAwait(false);
+            if (this.DetectStationary)
+                this.DetectIfStationary(reading);
+            
+            this.MostRecentReading = reading;
+            var fireReading = false;
+            if (this.LastReading == null)
+            {
+                fireReading = true;
+                this.Logger.LogDebug("No previous reading");
+            }
+            else
+            {
+                if (this.MinimumDistance != null)
+                {
+                    var dist = this.LastReading.Position.GetDistanceTo(reading.Position);
+                    fireReading = dist >= this.MinimumDistance;
+
+                    this.Logger.DeferDistanceInfo(this.MinimumDistance!.TotalMeters, dist.TotalMeters, fireReading);
+                }
+
+                if (!fireReading && this.MinimumTime != null)
+                {
+                    var timeDiff = reading.Timestamp.Subtract(this.LastReading.Timestamp);
+                    fireReading = timeDiff >= this.MinimumTime;
+
+                    this.Logger.DeferTimeInfo(this.MinimumTime!.Value, timeDiff, fireReading);
+                }
+            }
+
+            if (fireReading)
+            {
+                try
+                {
+                    await this.OnGpsReading(reading).ConfigureAwait(false);
+                }
+                finally
+                {
+                    this.LastReading = reading;
+                }
+            }
         }
-        else
+        finally
         {
-            if (this.MinimumDistance != null)
-            {
-                var dist = this.LastReading.Position.GetDistanceTo(reading.Position);
-                fireReading = dist >= this.MinimumDistance;
-
-                this.Logger.DeferDistanceInfo(this.MinimumDistance!.TotalMeters, dist.TotalMeters, fireReading);
-            }
-
-            if (!fireReading && this.MinimumTime != null)
-            {
-                var timeDiff = reading.Timestamp.Subtract(this.LastReading.Timestamp);
-                fireReading = timeDiff >= this.MinimumTime;
-
-                this.Logger.DeferTimeInfo(this.MinimumTime!.Value, timeDiff, fireReading);
-            }
-        }
-
-        if (fireReading)
-        {
-            try
-            {
-                await this.OnGpsReading(reading).ConfigureAwait(false);
-            }
-            finally
-            {
-                this.LastReading = reading;
-            } 
+            this.semaphore.Release();
         }
     }
 
+    
+    DateTimeOffset? lastMovement;
+    
+    internal void DetectIfStationary(GpsReading reading)
+    {
+        this.lastMovement ??= reading.Timestamp;
+
+        if (this.MostRecentReading != null)
+        {
+            var distance = reading.Position.GetDistanceTo(this.MostRecentReading.Position);
+            if (distance.TotalMeters < this.StationaryMetersThreshold)
+            {
+                var time = reading.Timestamp - this.lastMovement;
+                if (time.Value.TotalSeconds >= this.StationarySecondsThreshold)
+                {
+                    if (!this.IsStationary)
+                    {
+                        this.IsStationary = true;
+                        logger.LogDebug("GPS showing stationary");
+                    }
+                }
+            }
+            else
+            {
+                this.lastMovement = reading.Timestamp;
+                if (this.IsStationary)
+                {
+                    this.IsStationary = false;
+                    logger.LogDebug("GPS showing movement");
+                }
+            }
+        }
+    }
+    
+    
+    protected int StationaryMetersThreshold { get; set; } = 10;
+    protected int StationarySecondsThreshold { get; set; } = 30;
+    protected bool DetectStationary { get; set; }
+    
 
     GpsReading? lastReading;
+    /// <summary>
+    /// This is the last GPS reading before OnReading is raised
+    /// </summary>
     public GpsReading? LastReading
     {
         get => this.lastReading;
@@ -64,6 +115,29 @@ public abstract class GpsDelegate : NotifyPropertyChanged, IGpsDelegate
     }
 
 
+    bool isStationary;
+    /// <summary>
+    /// Will look at all 
+    /// </summary>
+    public bool IsStationary
+    {
+        get => this.isStationary;
+        private set => this.Set(ref this.isStationary, value);
+    }
+    
+
+    GpsReading? mostRecentReading;
+    /// <summary>
+    /// This is the most recent reading from the GPS outside of the filters
+    /// If you are gettings this as of OnReading, it will be the current incoming reading
+    /// </summary>
+    public GpsReading? MostRecentReading
+    {
+        get => this.mostRecentReading;
+        set => this.Set(ref this.mostRecentReading, value);
+    }
+    
+    
     Distance? minDistance;
     public Distance? MinimumDistance
     {
