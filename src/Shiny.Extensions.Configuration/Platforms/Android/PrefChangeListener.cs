705200dc37cc4d8c1d63468d626143945ab56289
Allan Ritchie <allan.ritchie@gmail.com>
2025-06-03 12:00:29 -0400

    Mini change to trigger version

--- src/Shiny.Extensions.Configuration/Platforms/Android/PrefChangeListener.cs
+++ src/Shiny.Extensions.Configuration/Platforms/Android/PrefChangeListener.cs
@@ -4,12 +4,8 @@ using System;
 namespace Shiny.Extensions.Configuration;
 
 
-public class PrefChangeListener : Java.Lang.Object, ISharedPreferencesOnSharedPreferenceChangeListener
+public class PrefChangeListener(Action action) : Java.Lang.Object, ISharedPreferencesOnSharedPreferenceChangeListener
 {
-    readonly Action action;
-    public PrefChangeListener(Action action) => this.action = action;
-
-
     public void OnSharedPreferenceChanged(ISharedPreferences? sharedPreferences, string? key)
-        => this.action();
+        => action();
 }
