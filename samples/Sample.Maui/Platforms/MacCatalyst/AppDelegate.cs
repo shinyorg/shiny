c5e200e79fa23955a15d78088219e87130713680
Allan Ritchie <allan.ritchie@gmail.com>
2026-05-17 21:41:14 -0400

    Sample Updates

--- samples/Sample.Maui/Platforms/MacCatalyst/AppDelegate.cs
+++ samples/Sample.Maui/Platforms/MacCatalyst/AppDelegate.cs
@@ -7,15 +7,15 @@ namespace Sample.Maui;
 public class AppDelegate : MauiUIApplicationDelegate
 {
     protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-    
+
     [Export("application:didRegisterForRemoteNotificationsWithDeviceToken:")]
     public void RegisteredForRemoteNotifications(UIApplication application, NSData deviceToken)
         => global::Shiny.Hosting.Host.Lifecycle.OnRegisteredForRemoteNotifications(deviceToken);
-  
+
     [Export("application:didFailToRegisterForRemoteNotificationsWithError:")]
     public void FailedToRegisterForRemoteNotifications(UIApplication application, NSError error)
         => global::Shiny.Hosting.Host.Lifecycle.OnFailedToRegisterForRemoteNotifications(error);
-  
+
     [Export("application:didReceiveRemoteNotification:fetchCompletionHandler:")]
     public void DidReceiveRemoteNotification(UIApplication application, NSDictionary userInfo, Action<UIBackgroundFetchResult> completionHandler)
         => global::Shiny.Hosting.Host.Lifecycle.OnDidReceiveRemoteNotification(userInfo, completionHandler);
---
+++ samples/Sample.Maui/Platforms/MacCatalyst/Program.cs
@@ -0,0 +1,11 @@
+using UIKit;
+
+namespace Sample.Maui;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        UIApplication.Main(args, null, typeof(AppDelegate));
+    }
+}
