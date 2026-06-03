106868540677f553e116e43528447b62a2c95b14
Allan Ritchie <allan.ritchie@gmail.com>
2026-06-03 17:42:40 -0400

    Serializer fixes

--- src/Shiny.Locations/ShinyLocationsJsonContext.cs
+++ src/Shiny.Locations/ShinyLocationsJsonContext.cs
@@ -2,5 +2,10 @@ using System.Text.Json.Serialization;
 
 namespace Shiny.Locations;
 
+#if APPLE
+[JsonSerializable(typeof(AppleGpsRequest))]
+#elif ANDROID
+[JsonSerializable(typeof(AndroidGpsRequest))]
+#endif
 [JsonSerializable(typeof(GeofenceRegion))]
 internal partial class ShinyLocationsJsonContext : JsonSerializerContext;
--- src/Shiny.Net.Http/ShinyHttpJsonContext.cs
+++ src/Shiny.Net.Http/ShinyHttpJsonContext.cs
@@ -2,5 +2,8 @@ using System.Text.Json.Serialization;
 
 namespace Shiny.Net.Http;
 
+#if APPLE
+[JsonSerializable(typeof(AppleHttpTransferRequest))]
+#endif
 [JsonSerializable(typeof(HttpTransfer))]
 public partial class ShinyHttpJsonContext : JsonSerializerContext;
